62c3f93a341b836f78db612e1688b74200af46d4
John Kelley <john@kelley.ca>
2013-09-23 10:29:20 -0700

    First stab at BLE DFU

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 # tool location
-BIN=~/Downloads/gcc-arm-none-eabi-4_7-2013q2/bin
+BIN=~/Work/gcc-arm-none-eabi-4_7-2013q1/bin
 #BIN=~/Downloads/gcc-arm-none-eabi-4_7-2012q4/bin
 JLINK_BIN=~/Work/jlink_462b
 
@@ -9,11 +9,13 @@ TARGET = bootloader.bin
 # nRF51822 revision (for PAN workarounds in nRF SDK)
 NRFREV = NRF51822_QFAA_CA
 
+	#$(wildcard ble/*.c) \
+
 SRCS =  $(wildcard *.c) \
-	$(wildcard ble/*.c) \
 	$(wildcard micro-ecc/*.c) \
 	nRF51_SDK/nrf51822/Source/templates/gcc/gcc_startup_nrf51.s \
 	nRF51_SDK/nrf51822/Source/templates/system_nrf51.c \
+	nRF51_SDK/nrf51422/Source/app_common/app_gpiote.c \
 	nRF51_SDK/nrf51822/Source/app_common/app_timer.c \
 	nRF51_SDK/nrf51822/Source/ble/ble_advdata.c \
 	nRF51_SDK/nrf51822/Source/ble/ble_bondmngr.c \
@@ -25,7 +27,9 @@ SRCS =  $(wildcard *.c) \
 	nRF51_SDK/nrf51822/Source/ble/ble_services/ble_dis.c \
 	nRF51_SDK/nrf51822/Source/ble/ble_services/ble_srv_common.c \
 	nRF51_SDK/nrf51822/Source/nrf_delay/nrf_delay.c \
-	nRF51_SDK/nrf51822/Source/simple_uart/simple_uart.c
+	nRF51_SDK/nrf51822/Source/ble/ble_services/ble_dfu.c \
+	nRF51_SDK/nrf51822/Source/app_common/app_scheduler.c \
+	nRF51_SDK/nrf51822/Source/simple_uart/simple_uart.c 
 
 INCS =  ./ \
 	./ble \
@@ -35,6 +39,7 @@ INCS =  ./ \
 	./nRF51_SDK/nrf51822/Include/gcc \
 	./nRF51_SDK/nrf51822/Include/ble \
 	./nRF51_SDK/nrf51822/Include/ble/ble_services/ \
+	./nRF51_SDK/nrf51822/Board/nrf6310/device_firmware_updates/bootloader/include/ \
 	./SoftDevice/s110_nrf51822_5.2.1_API/include
 
 # optimization flags
--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include "micro-ecc/ecc.h"
 #include <app_error.h>
+#include <app_gpiote.h>
 #include <nrf_gpio.h>
 #include <nrf_delay.h>
 #include <nrf_soc.h>
@@ -9,10 +10,20 @@
 #include <ble_bas.h>
 #include <simple_uart.h>
 #include "sha.h"
+#include <dfu_types.h>
+#include <dfu_transport.h>
+#include <nrf_sdm.h>
+#include <ble_dfu.h>
+
+#define IRQ_ENABLED             0x01           /**< Field identifying if an interrupt is enabled. */
+#define MAX_NUMBER_INTERRUPTS   32             /**< Maximum number of interrupts available. */
+
+#define APP_GPIOTE_MAX_USERS            2      /**< Number of GPIOTE users in total. Used by button module and dfu_transport_serial module (flow control). */
 
 extern void ble_init(void);
 extern void ble_advertising_start(void);
 extern uint32_t ble_services_update_battery_level(uint8_t level);
+extern uint32_t dfu_init_state(void);
 
 static app_timer_id_t m_battery_timer_id; /**< Battery timer. */
 
@@ -82,45 +93,127 @@ application_timers_start(void)
     APP_ERROR_CHECK(err_code);
 }
 
-void sha1_code_area(uint8_t *hash)
+static void
+sha1_fw_area(uint8_t *hash)
 {
-    NRF_UICR_Type *uicr = NRF_UICR;
-    NRF_FICR_Type *ficr = NRF_FICR;
-    uint32_t code_size = uicr->BOOTLOADERADDR - NRF_UICR_BASE; //!< fix
-    uint32_t code_address = NRF_UICR_BASE; //wrong
     SHA_CTX ctx;
-    
+    uint32_t code_size    = DFU_IMAGE_MAX_SIZE_FULL;
+    uint8_t *code_address = (uint8_t *)CODE_REGION_1_START;
+    uint32_t *index = (uint32_t *)BOOTLOADER_REGION_START - DFU_APP_DATA_RESERVED - 4;
+
+    // walk back to the end of the actual firmware
+    while (*index-- == EMPTY_FLASH_MASK && code_size > 0)
+        code_size -= 4;
 
     SHA1_Init(&ctx);
     SHA1_Update(&ctx, (void *)code_address, code_size);
     SHA1_Final(hash, &ctx);
 }
 
-bool
-verify_code_sha1(uint8_t *valid_hash)
+static bool
+verify_fw_sha1(uint8_t *valid_hash)
 {
     uint8_t sha1[SHA1_DIGEST_LENGTH];
-    uint8_t test[SHA1_DIGEST_LENGTH] = {0}; // replace this
     uint8_t comp = 0;
     int i = 0;
 
-    sha1_code_area(sha1);
+    sha1_fw_area(sha1);
 
     for (i = 0; i < SHA1_DIGEST_LENGTH; i++)
-        comp |= sha1[i] ^ test[i];
+        comp |= sha1[i] ^ valid_hash[i];
 
     return comp == 0;
+}
+
+/**@brief Function for disabling all interrupts before jumping from bootloader to application.
+ */
+static void
+interrupts_disable(void)
+{
+    uint32_t interrupt_setting_mask;
+    uint8_t  irq;
+
+    // We start the loop from first interrupt, i.e. interrupt 0.
+    irq                    = 0;
+    // Fetch the current interrupt settings.
+    interrupt_setting_mask = NVIC->ISER[0];
+    
+    for (; irq < MAX_NUMBER_INTERRUPTS; irq++)
+    {
+        if (interrupt_setting_mask & (IRQ_ENABLED << irq))
+        {
+            // The interrupt was enabled, and hence disable it.
+            NVIC_DisableIRQ((IRQn_Type) irq);
+        }
+    }        
+}
+
+static void
+boot_firmware(uint32_t addr) {
+    asm("LDR   R2, [R0]");//Get App MSP.
+    asm("MSR   MSP, R2"); //Set the main stack pointer to the applications MSP.
+    asm("LDR   R3, [R0, #0x00000004]"); //Get application reset vector address.
+    asm("BX    R3"); // No return - stack code is now activated only through SVC and plain interrupts.
+}
+
+void
+bootloader_dfu_update_process(dfu_update_status_t update_status) {
+    if (update_status.status_code == DFU_TIMEOUT)
+    {
+        // Timeout has occurred. Close the connection with the DFU Controller.
+        uint32_t err_code = dfu_transport_close();
+        APP_ERROR_CHECK(err_code);
+    }
 }
 
 void
 _start()
 {
+    //NRF_UICR_Type *uicr = NRF_UICR;
+    //NRF_FICR_Type *ficr = NRF_FICR;
 	uint32_t err_code;
+
+    // XXX: get signature blob from App Data section
+    uint8_t valid_fw[SHA1_DIGEST_LENGTH] = {0};
+
+    APP_TIMER_INIT(APP_TIMER_PRESCALER, APP_TIMER_MAX_TIMERS, APP_TIMER_OP_QUEUE_SIZE, true);
+    APP_GPIOTE_INIT(APP_GPIOTE_MAX_USERS);
+
+    //leds
+    nrf_gpio_cfg_output(21);
+    nrf_gpio_cfg_output(22);
+    nrf_gpio_cfg_output(23);
+
+    nrf_gpio_pin_set(21);
+    nrf_delay_ms(500);
+    // measure and verify current firmware
+    if (verify_fw_sha1(valid_fw)) {
+        //nrf_gpio_pin_set(22);
+        nrf_gpio_pin_clear(21);
+
+        // boot FW if valid
+        interrupts_disable();
+
+        uint32_t err_code = sd_softdevice_forward_to_application();
+        APP_ERROR_CHECK(err_code);
+        
+        boot_firmware(CODE_REGION_1_START);
+    } else {
+        nrf_gpio_pin_set(22);
+        nrf_gpio_pin_clear(21);
+        // else enter DFU over BLE mode
+        err_code = dfu_init_state(); 
+        if (err_code == NRF_SUCCESS)    
+        {
+            err_code = dfu_transport_update_start();
+        }
+    }
+    // XXX: implement recovery from external flash
 
 	/*
 	nrf_gpio_cfg_output(GPIO_3v3_Enable);
 	nrf_gpio_pin_set(GPIO_3v3_Enable);
-	*/
+	
 
 	simple_uart_config(0, 9, 0, 10, false);
 	//timers_init();
