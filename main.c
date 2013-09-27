f15e64b181779bd3397e298bf2ad3633d060fceb
Andre Pang <andre.pang@gmail.com>
2013-09-27 05:36:53 -0700

    Bootloader now boots into the application after a DFU if the application image has been validated

--- main.c
+++ main.c
@@ -12,6 +12,7 @@
 #include <bootloader.h>
 #include <dfu_types.h>
 #include "bootloader_util_arm.h"
+#include <ble_flash.h>
 
 extern void ble_init(void);
 extern void ble_advertising_start(void);
@@ -90,32 +91,34 @@ application_timers_start(void)
     APP_ERROR_CHECK(err_code);
 }
 
-void sha1_code_area(uint8_t *hash)
+static void
+sha1_fw_area(uint8_t *hash)
 {
-    NRF_UICR_Type *uicr = NRF_UICR;
-    // NRF_FICR_Type *ficr = NRF_FICR;
-    uint32_t code_size = uicr->BOOTLOADERADDR - NRF_UICR_BASE; //!< fix
-    uint32_t code_address = NRF_UICR_BASE; //wrong
-    SHA_CTX ctx;
+	SHA_CTX ctx;
+	uint32_t code_size    = DFU_IMAGE_MAX_SIZE_FULL;
+	uint8_t *code_address = (uint8_t *)CODE_REGION_1_START;
+	uint32_t *index = (uint32_t *)BOOTLOADER_REGION_START - DFU_APP_DATA_RESERVED - 4;
 
+	// walk back to the end of the actual firmware
+	while (*index-- == EMPTY_FLASH_MASK && code_size > 0)
+		code_size -= 4;
 
-    SHA1_Init(&ctx);
-    SHA1_Update(&ctx, (void *)code_address, code_size);
-    SHA1_Final(hash, &ctx);
+	SHA1_Init(&ctx);
+	SHA1_Update(&ctx, (void *)code_address, code_size);
+	SHA1_Final(hash, &ctx);
 }
 
-bool
-verify_code_sha1(uint8_t *valid_hash)
+static bool
+verify_fw_sha1(uint8_t *valid_hash)
 {
-    uint8_t sha1[SHA1_DIGEST_LENGTH];
-    uint8_t test[SHA1_DIGEST_LENGTH] = {0}; // replace this
+	uint8_t sha1[SHA1_DIGEST_LENGTH];
     uint8_t comp = 0;
     int i = 0;
 
-    sha1_code_area(sha1);
+    sha1_fw_area(sha1);
 
     for (i = 0; i < SHA1_DIGEST_LENGTH; i++)
-        comp |= sha1[i] ^ test[i];
+        comp |= sha1[i] ^ valid_hash[i];
 
     return comp == 0;
 }
@@ -127,6 +130,30 @@ verify_code_sha1(uint8_t *valid_hash)
 static void gpiote_init(void)
 {
 	APP_GPIOTE_INIT(APP_GPIOTE_MAX_USERS);
+}
+
+static void flash_page_erase(uint32_t * p_page)
+{
+    // Turn on flash erase enable and wait until the NVMC is ready.
+    NRF_NVMC->CONFIG = (NVMC_CONFIG_WEN_Een << NVMC_CONFIG_WEN_Pos);
+    while (NRF_NVMC->READY == NVMC_READY_READY_Busy)
+    {
+        // Do nothing.
+    }
+
+    // Erase page.
+    NRF_NVMC->ERASEPAGE = (uint32_t)p_page;
+    while (NRF_NVMC->READY == NVMC_READY_READY_Busy)
+    {
+        // Do nothing.
+    }
+
+    // Turn off flash erase enable and wait until the NVMC is ready.
+    NRF_NVMC->CONFIG = (NVMC_CONFIG_WEN_Ren << NVMC_CONFIG_WEN_Pos);
+    while (NRF_NVMC->READY == NVMC_READY_READY_Busy)
+    {
+        // Do nothing
+    }
 }
 
 void
@@ -156,25 +183,22 @@ _start()
 
 	//dfu_init();
 
-	err_code = bootloader_dfu_start();
+	volatile uint8_t* proposed_fw_sha1 = (uint8_t*)BOOTLOADER_SETTINGS_ADDRESS;
+	while(!verify_fw_sha1(proposed_fw_sha1)) {
+		err_code = bootloader_dfu_start();
+		APP_ERROR_CHECK(err_code);
+
+		uint8_t new_fw_sha1[SHA1_DIGEST_LENGTH];
+		sha1_fw_area(new_fw_sha1);
+
+		flash_page_erase((uint32_t*)BOOTLOADER_SETTINGS_ADDRESS);
+		ble_flash_block_write((uint32_t*)BOOTLOADER_SETTINGS_ADDRESS, (uint32_t*)new_fw_sha1, 5); // SHA1_DIGEST_LENGTH/sizeof(word)
+	}
+
+	interrupts_disable();
+
+	err_code = sd_softdevice_forward_to_application();
 	APP_ERROR_CHECK(err_code);
 
-	if(bootloader_app_is_valid(DFU_BANK_0_REGION_START))
-	{
-		bootloader_app_start(DFU_BANK_0_REGION_START);		
-	}
-
-	NVIC_SystemReset();
-/*
-	nrf_gpio_cfg_output(GPIO_3v3_Enable);
-	nrf_gpio_pin_set(GPIO_3v3_Enable);
-	nrf_gpio_cfg_output(GPIO_HRS_PWM);
-
-	while (1){
-		nrf_gpio_pin_set(GPIO_HRS_PWM);
-		nrf_delay_ms(500);
-		nrf_gpio_pin_clear(GPIO_HRS_PWM);
-		nrf_delay_ms(500);
-	}
-*/
+	StartApplication(CODE_REGION_1_START);
 }
