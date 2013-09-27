e8b519af789dfece79bc7bc3754fd9dd9fda35c4
Andre Pang <andre.pang@gmail.com>
2013-09-27 03:46:07 -0700

    Change APP_TIMER_INIT false -> true, and NRF_CLOCK_LFCLKSRC_XTAL_20_PPM to NRF_CLOCK_LFCLKSRC_SYNTH_250_PPM, which makes the bootloader code work on a real Band

--- main.c
+++ main.c
@@ -70,7 +70,7 @@ timers_init(void)
     uint32_t err_code;
 
     // Initialize timer module
-    APP_TIMER_INIT(APP_TIMER_PRESCALER, APP_TIMER_MAX_TIMERS, APP_TIMER_OP_QUEUE_SIZE, false);
+    APP_TIMER_INIT(APP_TIMER_PRESCALER, APP_TIMER_MAX_TIMERS, APP_TIMER_OP_QUEUE_SIZE, true);
 
     // Create timers
     err_code = app_timer_create(&m_battery_timer_id,
