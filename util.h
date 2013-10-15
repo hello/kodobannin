17c2f3c0b5ff7d80637b69f0dc76324a9d5601b2
John Kelley <john@kelley.ca>
2013-10-14 22:18:24 -0700

    Add DEBUG macro to util.h

--- util.h
+++ util.h
@@ -1,13 +1,16 @@
 #pragma once
 
-#ifdef DEBUG_SERIAL
+#ifdef DEBUG_SERIAL //=====================================
+#include <simple_uart.h>
 #define PRINT_HEX(a,b) serial_print_hex((uint8_t *)a,b)
 #define PRINTS(a) simple_uart_putstring((const uint8_t *)a)
 #define PRINTC(a) simple_uart_put(a)
-#else
+#else //---------------------------------------------------
 #define PRINT_HEX(a,b) {}
 #define PRINTS(a) {}
 #define PRINTC(a) {}
-#endif
+#endif //===================================================
+
+#define DEBUG(a,b) {PRINTS(a); PRINT_HEX(&b, sizeof(b)); PRINTC('\r'); PRINTC('\n');}
 
 void serial_print_hex(uint8_t *ptr, uint32_t len);
