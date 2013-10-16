3ca3f03098e75a8ce2dfcb65575e09ac294bb5e8
John Kelley <john@kelley.ca>
2013-10-15 21:31:58 -0700

    Add Data characteristic to Hello Demo Service

--- ble/services/ble_hello_demo.c
+++ ble/services/ble_hello_demo.c
@@ -44,6 +44,7 @@ dispatch_write(ble_evt_t *event) {
 void
 ble_hello_demo_on_ble_evt(ble_evt_t *event)
 {
+    DEBUG("event id: 0x", event->header.evt_id)
     switch (event->header.evt_id)
     {
         case BLE_GAP_EVT_CONNECTED:
@@ -129,35 +130,93 @@ on_data_fetch_cccd_write(ble_gatts_evt_write_t * p_evt_write)
  * @return      NRF_SUCCESS on success, otherwise an error code.
  */
 static uint32_t char_add(uint16_t                        uuid,
+                         bool                            disable_write,
                          uint8_t *                       p_char_value,
                          uint16_t                        char_len,
-                         const ble_srv_security_mode_t * dis_attr_md,
                          ble_gatts_char_handles_t *      p_handles)
 {
-    ble_uuid_t          ble_uuid;
     ble_gatts_char_md_t char_md;
+    ble_gatts_attr_md_t cccd_md;
     ble_gatts_attr_t    attr_char_value;
+    ble_uuid_t          ble_uuid;
     ble_gatts_attr_md_t attr_md;
-
-    APP_ERROR_CHECK_BOOL(p_char_value != NULL);
-    APP_ERROR_CHECK_BOOL(char_len > 0);
     
-    // The ble_gatts_char_md_t structure uses bit fields. So we reset the memory to zero.
+    memset(&cccd_md, 0, sizeof(cccd_md));
+
+    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&cccd_md.read_perm);
+    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&cccd_md.write_perm);// = p_dts_init->dts_data_fetch_attr_md.cccd_write_perm;
+    cccd_md.vloc = BLE_GATTS_VLOC_STACK;
+
     memset(&char_md, 0, sizeof(char_md));
-
-    char_md.char_props.read  = 1;
-    char_md.p_char_user_desc = NULL;
-    char_md.p_char_pf        = NULL;
-    char_md.p_user_desc_md   = NULL;
-    char_md.p_cccd_md        = NULL;
-    char_md.p_sccd_md        = NULL;
-
+    
+    char_md.char_props.notify       = 1;
+    char_md.p_char_user_desc        = NULL;
+    char_md.p_char_pf               = NULL;
+    char_md.p_user_desc_md          = NULL;
+    char_md.p_cccd_md               = &cccd_md;
+    char_md.p_sccd_md               = NULL;
+    
     BLE_UUID_BLE_ASSIGN(ble_uuid, uuid);
     
     memset(&attr_md, 0, sizeof(attr_md));
 
-    attr_md.read_perm  = dis_attr_md->read_perm;
-    attr_md.write_perm = dis_attr_md->write_perm;
+    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&attr_md.read_perm);//  = p_dts_init->dts_data_fetch_attr_md.read_perm;
+    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&attr_md.write_perm);// = p_dts_init->dts_data_fetch_attr_md.write_perm;
+    attr_md.vloc       = BLE_GATTS_VLOC_STACK;
+    attr_md.rd_auth    = 0;
+    attr_md.wr_auth    = 0;
+    attr_md.vlen       = 1;
+    
+    memset(&attr_char_value, 0, sizeof(attr_char_value));
+    
+    attr_char_value.p_uuid       = &ble_uuid;
+    attr_char_value.p_attr_md    = &attr_md;
+    attr_char_value.init_len     = char_len;
+    attr_char_value.init_offs    = 0;
+    attr_char_value.max_len      = char_len;
+    attr_char_value.p_value      = p_char_value;
+    
+    return sd_ble_gatts_characteristic_add(service_handle,
+                                        &char_md,
+                                        &attr_char_value,
+                                        p_handles);
+    /*ble_uuid_t          ble_uuid;
+    ble_gatts_char_md_t char_md;
+    ble_gatts_attr_md_t attr_md;
+    ble_gatts_attr_md_t cccd_md;
+    ble_gatts_attr_t    attr_char_value;
+    
+    APP_ERROR_CHECK_BOOL(p_char_value != NULL);
+    APP_ERROR_CHECK_BOOL(char_len > 0);
+    
+    // Setup cccd security modes
+    memset(&cccd_md, 0, sizeof(cccd_md));
+    memset(&char_md, 0, sizeof(char_md));
+    memset(&attr_md, 0, sizeof(attr_md));
+
+    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&attr_md.read_perm);
+    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&cccd_md.read_perm);
+    cccd_md.vloc = BLE_GATTS_VLOC_STACK;
+
+    // disabe writes
+    if (disable_write) {
+        BLE_GAP_CONN_SEC_MODE_SET_NO_ACCESS(&attr_md.write_perm);
+        
+    } else {
+        BLE_GAP_CONN_SEC_MODE_SET_OPEN(&attr_md.write_perm);
+    }
+
+    // The ble_gatts_char_md_t structure uses bit fields. So we reset the memory to zero.
+    //char_md.char_props.read   = 1;
+    char_md.char_props.notify = 1;
+    char_md.p_char_user_desc  = NULL;
+    char_md.p_char_pf         = NULL;
+    char_md.p_user_desc_md    = NULL;
+    char_md.p_cccd_md         = &cccd_md;
+    char_md.p_sccd_md         = NULL;
+
+    BLE_UUID_BLE_ASSIGN(ble_uuid, uuid);
+    
     attr_md.vloc       = BLE_GATTS_VLOC_STACK;
     attr_md.rd_auth    = 0;
     attr_md.wr_auth    = 0;
@@ -173,14 +232,14 @@ static uint32_t char_add(uint16_t                        uuid,
     attr_char_value.p_value      = p_char_value;
 
     return sd_ble_gatts_characteristic_add(service_handle, &char_md, &attr_char_value, p_handles);
-}
+*/}
 
 
 uint32_t ble_hello_demo_init(const ble_hello_demo_init_t *init) {
     uint32_t   err_code;
     ble_uuid_t ble_uuid;
     const ble_uuid128_t hello_uuid = {.uuid128 = BLE_UUID_HELLO_BASE};
-    uint8_t zeroes[20] = {};
+    uint8_t zeroes[20] = {0xAC};
 
     // Init defaults
     conn_handle = BLE_CONN_HANDLE_INVALID;
@@ -209,20 +268,20 @@ uint32_t ble_hello_demo_init(const ble_hello_demo_init_t *init) {
 
     // Add characteristics
     err_code = char_add(BLE_UUID_DATA_CHAR,
+                        0,
                         zeroes, 
                         BLE_GAP_DEVNAME_MAX_WR_LEN,
-                        &init->dis_attr_md,
                         &sys_data_handles);
     if (err_code != NRF_SUCCESS)
         return err_code;
-    
+    /*
     err_code = char_add(BLE_UUID_CONF_CHAR,
+                        0,
                         zeroes,
                         4,
-                        &init->dis_attr_md,
                         &sys_mode_handles);
     if (err_code != NRF_SUCCESS)
         return err_code;
-
+*/
     return NRF_SUCCESS;
 }
