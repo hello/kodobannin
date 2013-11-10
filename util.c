5c7d154a2ace20d86f50a12fe9e200847ab5a82a
John Kelley <john@sayhello.com>
2013-11-10 15:28:12 -0800

    syntax

--- util.c
+++ util.c
@@ -77,7 +77,8 @@ binary_to_hex(uint8_t *ptr, uint32_t len, uint8_t* out) {
 	}
 }
 
-void debug_uart_init()
+void
+debug_uart_init()
 {
     simple_uart_config(0, 5, 0, 8, false);
     uint32_t err_code = init_spi(SPI_Channel_0, SPI_Mode0, IMU_SPI_MISO, IMU_SPI_MOSI, IMU_SPI_SCLK, IMU_SPI_nCS);
