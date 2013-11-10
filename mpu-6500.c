8fac9b546962b322ddc6e6229e5613d32d5500e2
Andre Pang <andre.pang@gmail.com>
2013-11-10 12:51:16 -0800

    Add imu_accelerometer_continuous function, and commented-out imu_accelerometer_self_test function

--- mpu-6500.c
+++ mpu-6500.c
@@ -117,6 +117,105 @@ int mpu_read_mem(unsigned short mem_addr, unsigned short length,
     return 0;
 }
 */
+
+static void
+imu_accelerometer_continuous()
+{
+#define P_LSB16(p_int16) ((uint8_t*)(p_int16)+0)
+#define P_MSB16(p_int16) ((uint8_t*)(p_int16)+1)
+
+	int16_t values[3];
+
+	for(;;) {
+		register_read(MPU_REG_ACC_X_HI, (uint8_t*)&values[0]+1);
+		register_read(MPU_REG_ACC_X_LO, P_LSB16(&values[0]));
+		register_read(MPU_REG_ACC_Y_HI, P_LSB16(&values[1]));
+		register_read(MPU_REG_ACC_Y_LO, P_MSB16(&values[1]));
+		register_read(MPU_REG_ACC_Z_HI, P_LSB16(&values[2]));
+		register_read(MPU_REG_ACC_Z_LO, P_MSB16(&values[2]));
+
+		PRINT_HEX(&values[0], sizeof(values[0]));
+		PRINT_HEX(&values[1], sizeof(values[1]));
+		PRINT_HEX(&values[2], sizeof(values[2]));
+
+		PRINTS("\r\n");
+	}
+}
+
+/*
+static void imu_accelerometer_self_test()
+{
+	bool success = false;
+
+	//register_write(MPU_REG_ACC_CFG, ACCEL_CFG_SCALE_8G);
+
+	//register_write(MPU_REG_FIFO_EN, FIFO_EN_QUEUE_ACCEL);
+
+	// See page 11 of the MPU-6500 Register Map descriptions for this algorithm
+
+	uint8_t self_test_accels[3];
+	memset(self_test_accels, 0x55, sizeof(self_test_accels));
+
+	success = register_read(MPU_REG_ACC_SELF_TEST_X, &self_test_accels[0]);
+	APP_ERROR_CHECK(!success);
+
+	register_read(MPU_REG_ACC_SELF_TEST_Y, &self_test_accels[1]);
+	APP_ERROR_CHECK(!success);
+
+	register_read(MPU_REG_ACC_SELF_TEST_Z, &self_test_accels[2]);
+	APP_ERROR_CHECK(!success);
+
+	DEBUG("X/Y/Z self-test accels: ", self_test_accels);
+
+	imu_reset_fifo();
+
+	unsigned c;
+
+	// These factory_trims values are _HARD-CODED_ to Board9 (Andre's
+	// dev board), because calculating them properly requires
+	// real-number arithmetic (see page 12 of the register PDF). We'll
+	// need to figure out a way to do this properly if we want to do a
+	// real self-test.
+	uint8_t factory_trims[3] = { 15, 22, 37 };
+
+	DEBUG("X/Y/Z factory trims: ", factory_trims);
+
+#define SELF_TEST_SAMPLE_COUNT 50 // 100 Hz * 50 = .5 seconds
+
+	const unsigned sample_count = SELF_TEST_SAMPLE_COUNT * 3;
+
+	int16_t non_self_test_data[sample_count];
+	memset(non_self_test_data, 0x55, sizeof(non_self_test_data));
+	imu_fifo_read(sample_count*sizeof(int16_t), non_self_test_data);
+	DEBUG("non_self_test_data: ", non_self_test_data);
+
+	success = register_write(MPU_REG_ACC_CFG, AX_ST_EN|AY_ST_EN|AZ_ST_EN);
+	APP_ERROR_CHECK(!success);
+
+	int16_t self_test_data[sample_count];
+	memset(self_test_data, 0x55, sizeof(self_test_data));
+	imu_fifo_read(sample_count*sizeof(int16_t), self_test_data);
+	DEBUG("self_test_data: ", self_test_data);
+
+	int16_t self_test_responses[sample_count];
+	memset(self_test_responses, 0x55, sizeof(self_test_responses));
+	unsigned i;
+	for(i = 0, c = 0; i < sample_count; i++, c++) {
+		c = c == 2 ? 0 : c;
+
+		const uint16_t delta = self_test_data[i]-non_self_test_data[i];
+		self_test_responses[i] = delta;
+
+		if((delta >> 1) > factory_trims[c]) {
+			//DEBUG("WARNING: Acc self-test out of range: channel = ", c);
+			//DEBUG("WARNING: Acc self-test out of range: delta   = ", delta);
+		}
+	}
+
+	DEBUG("Self-test responses: ", self_test_responses);
+}
+*/
+
 uint32_t
 imu_init(enum SPI_Channel channel) {
 	uint32_t err;
