7cd10c019fe22261b02483600621fe5f3dd2006a
John Kelley <john@sayhello.com>
2013-11-10 17:47:09 -0800

    Fix MPU_REG_ACC_CFG to use correct Scale offset

--- mpu-6500.c
+++ mpu-6500.c
@@ -316,7 +316,7 @@ imu_init(enum SPI_Channel channel) {
 	// Init accel
 	PRINTS("Accel config\n");
 	buf[0] = SPI_Write(MPU_REG_ACC_CFG);
-	buf[1] = ACCEL_CFG_SCALE_8G;
+	buf[1] = (ACCEL_CFG_SCALE_8G << ACCEL_CFG_SCALE_OFFSET);
 	err = spi_xfer(chan, IMU_SPI_nCS, 2, buf, buf);
 	imu_uart_debug(err, buf, 2);
 	PRINTC('\n');
