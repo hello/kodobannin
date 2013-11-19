d66305f2968178f7a3a43bc3f6f42eafb765e30a
Andre Pang <andre.pang@gmail.com>
2013-11-18 17:50:11 -0800

    Remove gap_params_init() from ble_core.c

--- ble/ble_core.c
+++ ble/ble_core.c
@@ -106,37 +106,6 @@ ble_stack_init(void)
 				false);
 }
 
-/**@brief GAP initialization.
-*
-* @details This function shall be used to setup all the necessary GAP (Generic Access Profile)
-*          parameters of the device. It also sets the permissions and appearance.
-*/
-static void
-gap_params_init(void)
-{
-	uint32_t                err_code;
-	ble_gap_conn_params_t   gap_conn_params;
-	ble_gap_conn_sec_mode_t sec_mode;
-
-	BLE_GAP_CONN_SEC_MODE_SET_OPEN(&sec_mode);
-
-	err_code = sd_ble_gap_device_name_set(&sec_mode, (const uint8_t *)BLE_DEVICE_NAME, strlen(BLE_DEVICE_NAME));
-	APP_ERROR_CHECK(err_code);
-
-	err_code = sd_ble_gap_appearance_set(BLE_APPEARANCE_UNKNOWN);
-	APP_ERROR_CHECK(err_code);
-
-	memset(&gap_conn_params, 0, sizeof(gap_conn_params));
-
-	gap_conn_params.min_conn_interval = MIN_CONN_INTERVAL;
-	gap_conn_params.max_conn_interval = MAX_CONN_INTERVAL;
-	gap_conn_params.slave_latency     = SLAVE_LATENCY;
-	gap_conn_params.conn_sup_timeout  = CONN_SUP_TIMEOUT;
-
-	err_code = sd_ble_gap_ppcp_set(&gap_conn_params);
-	APP_ERROR_CHECK(err_code);
-}
-
 void
 ble_init()
 {
