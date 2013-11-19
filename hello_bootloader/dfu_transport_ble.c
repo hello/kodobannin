d862e34b013fd4ab9a201d4c883569cc1196c5a6
Andre Pang <andre.pang@gmail.com>
2013-11-18 17:52:19 -0800

    Remove gap_params_init() from dfu_transport_ble.c, and make it use the implementation from ble_gap.c

--- hello_bootloader/dfu_transport_ble.c
+++ hello_bootloader/dfu_transport_ble.c
@@ -671,36 +671,6 @@ static void ble_stack_init(void)
 }
 
 
-/**@brief   Function for the GAP initialization.
- *
- * @details This function will setup all the necessary GAP (Generic Access Profile)
- *          parameters of the device. It also sets the permissions and appearance.
- */
-static void gap_params_init(void)
-{
-    uint32_t                err_code;
-    ble_gap_conn_params_t   gap_conn_params;
-    ble_gap_conn_sec_mode_t sec_mode;
-
-    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&sec_mode);
-
-    err_code = sd_ble_gap_device_name_set(&sec_mode,
-                                          (const uint8_t *)DEVICE_NAME,
-                                          strlen(DEVICE_NAME));
-    APP_ERROR_CHECK(err_code);
-
-    memset(&gap_conn_params, 0, sizeof(gap_conn_params));
-
-    gap_conn_params.min_conn_interval = MIN_CONN_INTERVAL;
-    gap_conn_params.max_conn_interval = MAX_CONN_INTERVAL;
-    gap_conn_params.slave_latency     = SLAVE_LATENCY;
-    gap_conn_params.conn_sup_timeout  = CONN_SUP_TIMEOUT;
-
-    err_code = sd_ble_gap_ppcp_set(&gap_conn_params);
-    APP_ERROR_CHECK(err_code);
-}
-
-
 /**@brief   Function for the Advertising functionality initialization.
  *
  * @details Encodes the required advertising data and passes it to the stack.
@@ -812,6 +782,7 @@ static void radio_notification_init(void)
     APP_ERROR_CHECK(err_code);
 }
 
+extern void ble_gap_params_init(void);
 
 uint32_t dfu_transport_update_start()
 {
@@ -820,7 +791,7 @@ uint32_t dfu_transport_update_start()
     // Initialize the S110 Stack.
     ble_stack_init();
     scheduler_init();
-    gap_params_init();
+    ble_gap_params_init();
     services_init();
     advertising_init();
     conn_params_init();
