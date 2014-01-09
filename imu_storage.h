9f244bd6f74f5914ba07769a103068d2da6472ec
Andre Pang <andre.pang@gmail.com>
2014-01-09 15:00:42 -0800

    Add imu_version_header that contains a version number, and rename imu_data_header to imu_data_header_0

--- imu_storage.h
+++ imu_storage.h
@@ -2,7 +2,11 @@
 
 #pragma once
 
-struct imu_data_header {
+struct imu_version_header {
+    uint8_t version; //< version number. If this is 255, expect a secondary version number to follow, which should be added to this version number to get the real version. Use struct imu_data_header_N" to parse the version number.
+} __attribute__((packed));
+
+struct imu_data_header_0 {
 	uint32_t timestamp;  //< number of seconds since Jan 1, 2014.
 	bool gyro_xyz:1; //< If true, data has gyro XYZ values.
 	bool accel_xyz:1; //< If true, data has accelerometer XYZ values.
