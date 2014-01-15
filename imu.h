a1ce99971573272f33c10209df045e4cc04ec811
Andre Pang <andre.pang@gmail.com>
2014-01-14 18:21:07 -0800

    Add imu_wake_on_motion() and imu_did_wake_on_motion() functions to imu.h

--- imu.h
+++ imu.h
@@ -8,6 +8,21 @@ enum imu_sensor_set {
     IMU_SENSORS_GYRO = 2,
 };
 
+enum imu_wake_on_motion_hz {
+	IMU_WOM_HZ_0_25 = 0,
+	IMU_WOM_HZ_0_49 = 1,
+    IMU_WOM_HZ_0_98 = 2,
+    IMU_WOM_HZ_1_95 = 3,
+    IMU_WOM_HZ_3_91 = 4,
+    IMU_WOM_HZ_7_81 = 5,
+    IMU_WOM_HZ_15_63 = 6,
+    IMU_WOM_HZ_31_25 = 7,
+    IMU_WOM_HZ_62_50 = 8,
+    IMU_WOM_HZ_125 = 9,
+    IMU_WOM_HZ_250 = 10,
+    IMU_WOM_HZ_500 = 11,
+};
+
 void imu_init(enum SPI_Channel channel);
 
 uint16_t imu_accel_reg_read(uint8_t *buf);
@@ -21,6 +36,9 @@ enum imu_sensor_set imu_get_sensors();
 uint16_t imu_fifo_bytes_available();
 uint16_t imu_fifo_read(uint16_t count, uint8_t *buf);
 void imu_fifo_clear();
+
+void imu_wake_on_motion(uint16_t microgravities, enum imu_wake_on_motion_hz wom_hz);
+bool imu_did_wake_on_motion();
 
 uint32_t imu_timer_ticks_from_sample_rate(uint8_t hz);
 
