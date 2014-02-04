5f435dce52373e7f226969e673118efb5a7ff022
Andre Pang <andre.pang@gmail.com>
2014-02-04 13:39:25 -0800

    Add version number to imu_data_header

--- imu_storage.h
+++ imu_storage.h
@@ -3,6 +3,7 @@
 #pragma once
 
 struct imu_data_header {
+	uint8_t version; // version number: should always be 00
 	uint32_t timestamp;  //< number of seconds since Jan 1, 2014.
 	bool gyro_xyz:1; //< If true, data has gyro XYZ values.
 	bool accel_xyz:1; //< If true, data has accelerometer XYZ values.
