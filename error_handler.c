cf2efbcdd6c3dd3a4729c2cbd115f810a1157bef
Andre Pang <andre.pang@gmail.com>
2014-02-11 16:41:44 -0800

    Useless comments--

--- error_handler.c
+++ error_handler.c
@@ -6,12 +6,6 @@
 #include "device_params.h"
 #include "hello_dfu.h"
 
-/**@brief Error handler function, which is called when an error has occurred.
- *
- * @param[in] error_code  Error code supplied to the handler.
- * @param[in] line_num    Line number where the handler is called.
- * @param[in] p_file_name Pointer to the file name.
- */
 void
 app_error_handler(uint32_t error_code, uint32_t line_num, const uint8_t *filename)
 {
@@ -53,17 +47,6 @@ app_error_handler(uint32_t error_code, uint32_t line_num, const uint8_t *filenam
 	}
 }
 
-/**@brief Assert macro callback function.
- *
- * @details This function will be called in case of an assert in the SoftDevice.
- *
- * @warning This handler is an example only and does not fit a final product. You need to analyze
- *          how your product is supposed to react in case of Assert.
- * @warning On assert from the SoftDevice, the system can only recover on reset.
- *
- * @param[in]   line_num   Line number of the failing ASSERT call.
- * @param[in]   file_name  File name of the failing ASSERT call.
- */
 void
 assert_nrf_callback(uint16_t line_num, const uint8_t * p_file_name)
 {
