36cfd08eaf5a5f4a6cd88ed407b12c8c6adde7ff
Andre Pang <andre.pang@gmail.com>
2014-06-09 16:43:39 -0700

    Formatting fixes for aigz.c

--- drivers/aigz.c
+++ drivers/aigz.c
@@ -6,24 +6,24 @@
 #include "util.h"
 
 enum {
-  RTC_ADDRESS_WRITE = (0b1101000 << 1),
-  RTC_ADDRESS_READ = (0b1101000 << 1) | TWI_READ_BIT,
+    RTC_ADDRESS_WRITE = (0b1101000 << 1),
+    RTC_ADDRESS_READ = (0b1101000 << 1) | TWI_READ_BIT,
 };
 
 void
 rtc_read(struct rtc_time_t* time)
 {
-  uint8_t RTC_CLOCK_SECTION_ADDRESS = 0x0;
+    uint8_t RTC_CLOCK_SECTION_ADDRESS = 0x0;
 
-  BOOL_OK(twi_master_transfer(RTC_ADDRESS_WRITE, &RTC_CLOCK_SECTION_ADDRESS, sizeof(RTC_CLOCK_SECTION_ADDRESS), TWI_ISSUE_STOP));
-  BOOL_OK(twi_master_transfer(RTC_ADDRESS_READ, time->bytes, sizeof(time->bytes), TWI_ISSUE_STOP));
+    BOOL_OK(twi_master_transfer(RTC_ADDRESS_WRITE, &RTC_CLOCK_SECTION_ADDRESS, sizeof(RTC_CLOCK_SECTION_ADDRESS), TWI_ISSUE_STOP));
+    BOOL_OK(twi_master_transfer(RTC_ADDRESS_READ, time->bytes, sizeof(time->bytes), TWI_ISSUE_STOP));
 }
 
 void
 rtc_write(struct rtc_time_t* time)
 {
-  uint8_t RTC_CLOCK_SECTION_ADDRESS = 0x0;
+    uint8_t RTC_CLOCK_SECTION_ADDRESS = 0x0;
 
-  BOOL_OK(twi_master_transfer(RTC_ADDRESS_WRITE, &RTC_CLOCK_SECTION_ADDRESS, sizeof(RTC_CLOCK_SECTION_ADDRESS), TWI_ISSUE_STOP));
-  BOOL_OK(twi_master_transfer(RTC_ADDRESS_WRITE, time->bytes, sizeof(time->bytes), TWI_ISSUE_STOP));
+    BOOL_OK(twi_master_transfer(RTC_ADDRESS_WRITE, &RTC_CLOCK_SECTION_ADDRESS, sizeof(RTC_CLOCK_SECTION_ADDRESS), TWI_ISSUE_STOP));
+    BOOL_OK(twi_master_transfer(RTC_ADDRESS_WRITE, time->bytes, sizeof(time->bytes), TWI_ISSUE_STOP));
 }
