f5848646593016fc6670ee39076c63d53b55b4b3
Andre Pang <andre.pang@gmail.com>
2014-06-10 16:58:09 -0700

    Add hlo_ble_time_printf() function, for debugging

---
+++ common/hlo_ble_time.c
@@ -0,0 +1,8 @@
+// vi:noet:sw=4 ts=4
+
+#include "hlo_ble_time.h"
+
+void hlo_ble_time_printf(struct hlo_ble_time* ble_time)
+{
+    printf("%d/%d/%d %d:%d:%d", ble_time->month, ble_time->day, ble_time->year, ble_time->hours, ble_time->minutes, ble_time->seconds);
+}
