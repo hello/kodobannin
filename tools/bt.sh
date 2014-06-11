eaaa92e217be063ff5db52393c9dbff6d583e793
Andre Pang <andre.pang@gmail.com>
2014-06-11 15:49:12 -0700

    Added bt.sh script to restart BLE

--- tools/bt.sh
+++ tools/bt.sh
@@ -1,11 +1,3 @@
-sudo kextunload -b com.apple.iokit.BroadcomBluetoothHostControllerUSBTransport
-sudo kextunload -b com.apple.iokit.IOBluetoothSerialManager
-sudo kextunload -b com.apple.iokit.IOBluetoothUSBDFU
-sudo kextunload -b com.apple.iokit.IOBluetoothHostControllerUSBTransport
-sudo kextunload -b com.apple.iokit.IOBluetoothFamily
+#!/bin/sh
 
-sudo kextload -b com.apple.iokit.IOBluetoothFamily
-sudo kextload -b com.apple.iokit.IOBluetoothHostControllerUSBTransport
-sudo kextload -b com.apple.iokit.IOBluetoothUSBDFU
-sudo kextload -b com.apple.iokit.IOBluetoothSerialManager
-sudo kextload -b com.apple.iokit.BroadcomBluetoothHostControllerUSBTransport
+./blueutil off && ./blueutil on
