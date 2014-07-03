366086bf8bf786699f20b998d8fff926d3aedf6d
Jackson Chen <jchen@Jacksons-MacBook-Pro.local>
2014-07-02 17:59:30 -0700

    Initial commit of message_base protocol
    * bidirectional star network of various transports to route messages

---
+++ common/message_base.h
@@ -0,0 +1,50 @@
+/**
+ * Messaging framework type declarations.
+ *
+ */
+
+#include <stdbool.h>
+#include <stdint.h>
+#ifndef MESSAGE_H
+#define MESSAGE_H
+
+typedef struct{
+    uint32_t len;
+    void ( *dtor )(MSG_Data_t*);
+    uint8_t  buf[];
+}MSG_Data_t;
+
+typedef enum{
+    SLAVE = 0,
+    MASTER
+}MSG_Role;
+
+typedef enum{
+    MEM = 0,
+    UART,
+    SPI,
+    BTLE,
+    ANT
+}MSG_PipeType;
+
+typedef enum{
+    SUCCESS = 0,
+    FAIL,
+}MSG_Status;
+
+/**
+ * Message object.
+ * All modules that implements message capability must define the struct
+ * including this one( which other modules use to push message)
+ */
+typedef struct{
+    MSG_PipeType type;
+    MSG_Role role;
+    MSG_Status ( *init ) ( void );
+    MSG_Status ( *destroy ) ( void );
+    MSG_Status ( *flush ) ( void );
+    //probably need a source parameter
+    MSG_Status ( *queue ) (  MSG_Data_t * data  );
+}MSG_Base_t;
+
+#endif
