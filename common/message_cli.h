4f109b6569ff7df4e6c6d32caa163e6ca6dce6f9
Jackson Chen <jchen@Jacksons-MacBook-Pro.local>
2014-07-09 12:51:16 -0700

    Reworked cli

--- common/message_cli.h
+++ common/message_cli.h
@@ -4,12 +4,4 @@
 #define MSG_CLI_MAX_COMMANDS 5
 #define MSG_CLI_MAX_NAME_LEN 8
 
-typedef struct{
-
-}MSG_Cli_Params_t;
-typedef MSG_Status ( * msg_cli_handler) (const MSG_Cli_Params_t * param, char * out_opt_str);
-
-MSG_Status MSG_Cli_Initialize(void);
-MSG_Status MSG_Cli_Register_Command(const char * name, msg_cli_handler handler);
-MSG_Status MSG_Cli_Exec(const MSG_Data_t * d);
 
