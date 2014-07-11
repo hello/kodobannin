5b17613bc001de685b1cc880ef5be3aea569d82c
Jackson Chen <jchen@Jacksons-MacBook-Pro.local>
2014-07-11 14:39:44 -0700

    Removed comments

--- pill/timedfifo.c
+++ pill/timedfifo.c
@@ -1,40 +1,11 @@
 #include "timedfifo.h"
 #include "util.h"
-/**
-typedef struct{
-    uint8_t version;
-    uint8_t reserved_1;
-    uint16_t length;
-    uint8_t seconds;
-    uint8_t minutes;
-    uint8_t hours;
-    uint8_t day;
-    uint8_t month;
-    uint8_t reserved_2;
-    uint16_t year;
-    tf_unit_t data[TF_BUFFER_SIZE];
-}tf_data_t;
 
-struct hlo_ble_time
-{
-    union {
-        struct {
-            uint16_t year; // 1582-9999
-            uint8_t month; // 1-12, or 0 (unknown)
-            uint8_t day; // 1-31, or 0 (unknown)
-            uint8_t hours; // 0-23
-            uint8_t minutes; // 0-59
-            uint8_t seconds; // 0-59
-            uint8_t weekday; // 0-7; 1-7=Mon-Sun, 0=Unknown
-        } __attribute__((packed));
-        uint8_t bytes[8];
-    } __attribute__((packed));
-} __attribute__((packed));
-*/
 static struct{
     tf_data_t data;
     uint16_t tickctr;
 }self;
+
 void TF_Initialize(const struct hlo_ble_time * init_time){
     self.data.length = sizeof(self);
     self.data.seconds = init_time->seconds;
