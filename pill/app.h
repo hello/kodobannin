7bb12e7d2b1f8a0ed313d02cd79afff3f437f0bc
Pang Wu <pang@sayhello.com>
2014-08-13 15:38:28 -0700

    get the app setting back to integration mode

--- pill/app.h
+++ pill/app.h
@@ -32,7 +32,7 @@ enum {
 #define ANT_PREFER_NETWORK  0x0
 #define ANT_PREFER_CHTYPE   0x10
 //#define BONDING_REQUIRED
-#define IN_MEMORY_BONDING
+
 
 /*
  * BLE Connection Parameters
@@ -50,16 +50,16 @@ enum {
 #define SECOND_10_MS_UNITS                   100
 // Minimum acceptable connection interval (0.5 seconds),
 // Connection interval uses 1.25 ms units
-#define MIN_CONN_INTERVAL                    (4 * TWENTY_MS_1_25_MS_UNITS)
+#define MIN_CONN_INTERVAL                    (20 * TWENTY_MS_1_25_MS_UNITS)
 // Maximum acceptable connection interval (1 second), Connection interval uses 1.25 ms units.
-#define MAX_CONN_INTERVAL                    (4 * TWENTY_MS_1_25_MS_UNITS)
+#define MAX_CONN_INTERVAL                    (20 * TWENTY_MS_1_25_MS_UNITS)
 
 // Slave latency. */
-#define SLAVE_LATENCY                        1
+#define SLAVE_LATENCY                        4
 // Connection supervisory timeout (4 seconds), Supervision Timeout uses 10 ms units
 #define CONN_SUP_TIMEOUT                     (2 * MAX_CONN_INTERVAL * (SLAVE_LATENCY + 1))
 
-#define TX_POWER_LEVEL						(-4)
+#define TX_POWER_LEVEL						(-8)
 
 // Time from initiating event (connect or start of notification) to first
 // time ble_gap_conn_param_update is called (5 seconds)
