bed4e4c997df10aee1e471d98b2a737efa610c26
Pang Wu <pang@sayhello.com>
2014-08-28 13:35:11 -0700

    Shake detection modification

--- pill/shake_detect.c
+++ pill/shake_detect.c
@@ -1,24 +1,55 @@
 #include "shake_detect.h"
 #include "util.h"
 
+#define SLIDING_WINDOW_SIZE_SEC         (5)
+#define SHAKING_DATA_COUNT_THRESHOLD    (25)
+
 static struct{
-    uint32_t thmag, thcnt, cnt;
+    uint32_t thmag, cnt;
+    uint8_t shaking_time_sec;
 }self;
 
-void ShakeDetectReset(uint32_t threshold, uint32_t count){
+static shake_detection_callback_t _shake_detection_callback;
+
+void ShakeDetectReset(uint32_t threshold){
     self.thmag = threshold;
-    self.thcnt = count;
-    self.cnt = count;
+    self.cnt = 0;
+    self.shaking_time_sec = 0;
 }
+
+
 void ShakeDetectDecWindow(void){
     self.cnt = self.cnt >> 1;
-}
-bool ShakeDetect(uint32_t accelmag){
-    if(accelmag >= self.thmag){
-        if(++self.cnt >= self.thcnt){
-            self.cnt = 0;
-            return true;
+    if(self.shaking_time_sec){
+        ++self.shaking_time_sec;
+    }
+
+    if(self.shaking_time_sec > SLIDING_WINDOW_SIZE_SEC){
+        // sliding window ends, reset all the counter
+        uint32_t data_above_threshold = self.cnt;
+        self.shaking_time_sec = 0;
+        self.cnt = 0;
+
+        if(data_above_threshold >= SHAKING_DATA_COUNT_THRESHOLD){ // The user shakes hard enough and long enough
+            if(_shake_detection_callback){
+                _shake_detection_callback();
+            }
         }
     }
+}
+
+bool ShakeDetect(uint32_t accelmag){
+    if(accelmag >= self.thmag){
+        if(!self.shaking_time_sec){
+            ++self.shaking_time_sec;  // initialize the sliding window timer.
+        }
+
+        ++self.cnt;
+    }
+
     return false;
+}
+
+void set_shake_detection_callback(shake_detection_callback_t callback){
+    _shake_detection_callback = callback;
 }
