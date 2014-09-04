ffd87fb58c19e8bb3b4e030c6da7da5b04d3ec09
Jackson Chen <jchen@hello.is>
2014-09-04 12:50:47 -0700

    comment

--- morpheus/ant_user.h
+++ morpheus/ant_user.h
@@ -1,4 +1,8 @@
 #pragma once
 #include "message_ant.h"
+/**
+ * Application specific logic for message_ant module
+ * DO NOT block any of the callbacks, dispatch actions via central instead
+ */
 
 MSG_ANTHandler_t * morpheus_ant_user(MSG_Central_t * central);
