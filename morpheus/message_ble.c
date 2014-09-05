5272eb52e282fab5a43c1cb329ee56d1ef7a7c7e
Pang Wu <pang@sayhello.com>
2014-09-05 16:52:45 -0700

    Also clear the resource when timeout.

--- morpheus/message_ble.c
+++ morpheus/message_ble.c
@@ -339,3 +339,12 @@ MSG_Base_t* MSG_BLE_Base(MSG_Central_t* parent){
 
 }
 
+void clear_pill_pairing_state()
+{
+#ifdef ANT_ENABLE
+    ANT_UserSetPairing(0);
+#endif
+    _release_pending_resources();
+
+}
+
