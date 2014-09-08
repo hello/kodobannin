f42f87075b98c59e879cc5d3be07900fca1781e1
Jackson Chen <jchen@hello.is>
2014-09-08 13:40:20 -0700

    Import message_config to resolve build conflicts

--- bootloader/message_config.h
+++ bootloader/message_config.h
@@ -1,10 +1,22 @@
 #pragma once
+
+#include "platform.h"
+
+#ifdef ANT_STACK_SUPPORT_REQD
+#include <ant_stack_handler_types.h>
+#endif
+
 /*
  * Here define the modules used
  */
 
-#define MSG_BASE_SHARED_POOL_SIZE 10
-#define MSG_BASE_DATA_BUFFER_SIZE 31
+#define MSG_BASE_SHARED_POOL_SIZE 16
+#define MSG_BASE_DATA_BUFFER_SIZE (8 * sizeof(uint32_t))
+
+#ifdef MSG_BASE_USE_BIG_POOL
+#define MSG_BASE_SHARED_POOL_SIZE_BIG 6
+#define MSG_BASE_DATA_BUFFER_SIZE_BIG 256
+#endif
 
 #define MSG_CENTRAL_MODULE_NUM  10
 
@@ -16,6 +28,13 @@ typedef enum{
     ANT,
     RTC,
     CLI,
-    TIME
+    TIME,
+    SSPI
 }MSG_ModuleType;
+
+#ifdef ANT_STACK_SUPPORT_REQD
+void ant_handler(ant_evt_t * p_ant_evt);
+
+#define NUM_ANT_CHANNELS 2
+#endif
 
