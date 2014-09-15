e7f66732a588ac9bc45fdd0c9c99763a58522d6a
Jackson Chen <jchen@hello.is>
2014-09-15 15:29:01 -0700

    DFU to use platform specific names

--- bootloader/dfu_transport_ble.c
+++ bootloader/dfu_transport_ble.c
@@ -34,14 +34,11 @@
 #include "hci_mem_pool.h"
 #include <stddef.h>
 #include <string.h>
+#include "dfu_config.h"
 
 #define ADVERTISING_LED_PIN_NO               LED_0                                                   /**< Is on when device is advertising. */
 #define CONNECTED_LED_PIN_NO                 LED_1                                                   /**< Is on when device has connected. */
 #define ASSERT_LED_PIN_NO                    LED_7                                                   /**< Is on when application has asserted. */
-
-
-#define DEVICE_NAME                          "DfuTarg"                                               /**< Name of device. Will be included in the advertising data. */
-#define MANUFACTURER_NAME                    "NordicSemiconductor"                                   /**< Manufacturer. Will be passed to Device Information Service. */
 
 #define MIN_CONN_INTERVAL                    (uint16_t)(MSEC_TO_UNITS(11.25, UNIT_1_25_MS))          /**< Minimum acceptable connection interval (11.25 milliseconds). */
 #define MAX_CONN_INTERVAL                    (uint16_t)(MSEC_TO_UNITS(15, UNIT_1_25_MS))             /**< Maximum acceptable connection interval (15 milliseconds). */
