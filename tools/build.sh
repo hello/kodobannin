89a528f5f15be7f43403a15fc2836953fff9b078
Jackson Chen <jchen@hello.is>
2014-09-18 16:43:40 -0700

    Change path of buildlist

--- tools/build.sh
+++ tools/build.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 set -ex
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # make sure build_list exists
-builds=$DIR/build_list.txt
+builds=./tools/build_list.txt
 clean_repo()
 {
     make clean
