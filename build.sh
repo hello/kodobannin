21cf59db218a80b632406ed27da1fa071b62b103
Jackson Chen <jchen@hello.is>
2014-09-18 11:22:15 -0700

    Build common binaries

--- build.sh
+++ build.sh
@@ -1,6 +1,19 @@
-
+set -e
+builds=build_list.txt
 function sync_repo()
 {
     git submodule update && git submodule sync
 }
-sync_repo && echo "ok"
+function clean_repo()
+{
+    make clean
+}
+function build_targ()
+{
+    make $1
+}
+
+sync_repo && clean_repo
+while read p; do
+    build_targ $p
+done < $builds
