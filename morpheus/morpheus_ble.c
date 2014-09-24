9defed06ea912e4876f9d95a9bc74c57c55fe4da
Jackson Chen <jchen@hello.is>
2014-09-24 13:48:22 -0700

    Test code on morpheus

--- morpheus/morpheus_ble.c
+++ morpheus/morpheus_ble.c
@@ -29,6 +29,8 @@
 #include "ant_devices.h"
 #include "ant_bondmgr.h"
 #include "ant_user.h"
+#include "ant_driver.h"
+#include "ant_packet.h"
 
 // To generate the protobuf download nanopb
 // Generate C code: ~/nanopb-0.2.8-macosx-x86/generator-bin/protoc --nanopb_out=. morpheus/morpheus_ble.proto
@@ -378,20 +380,23 @@ void morpheus_load_modules(void){
 #endif
 		central->loadmod(MSG_BLE_Base(central));
 #ifdef ANT_ENABLE
-		central->loadmod(MSG_ANT_Base(central, ANT_UserInit(central)));
-
-		//MSG_Base_BufferTest();
-		MSG_SEND_CMD(central, CENTRAL, MSG_AppCommand_t, APP_LSMOD,NULL,0);
-		{
-			uint8_t role = ANT_DISCOVERY_CENTRAL;
-			MSG_SEND_CMD(central, ANT, MSG_ANTCommand_t, ANT_SET_ROLE, &role,1);
-		}
-		{
-			ANT_BondMgrInit();
-			ANT_BondMgrForEach(create_bond);
-		}
+/*
+ *        central->loadmod(MSG_ANT_Base(central, ANT_UserInit(central)));
+ *
+ *        //MSG_Base_BufferTest();
+ *        MSG_SEND_CMD(central, CENTRAL, MSG_AppCommand_t, APP_LSMOD,NULL,0);
+ *        {
+ *            uint8_t role = ANT_DISCOVERY_CENTRAL;
+ *            MSG_SEND_CMD(central, ANT, MSG_ANTCommand_t, ANT_SET_ROLE, &role,1);
+ *        }
+ *        {
+ *            ANT_BondMgrInit();
+ *            ANT_BondMgrForEach(create_bond);
+ *        }
+ */
 #endif
 
+        hlo_ant_init(HLO_ANT_ROLE_CENTRAL, hlo_ant_packet_init(NULL));
     }else{
         PRINTS("FAIL");
     }
