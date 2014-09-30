edd1cda2eb66c1cc658a1a6243157745259f4b3f
Jackson Chen <jchen@hello.is>
2014-09-29 18:33:56 -0700

    Added cli for pill

---
+++ pill/cli_user.c
@@ -0,0 +1,41 @@
+#include "cli_user.h"
+#include "util.h"
+
+static struct{
+    //parent is the reference to the dispatcher 
+    MSG_Central_t * parent;
+    MSG_CliUserListener_t listener;
+}self;
+
+static int
+_strncmp(const char * s1, const char *s2, uint32_t n){
+    while(n--){
+        if(*s1++!=*s2++){
+            return *(uint8_t*)(s1-1) - *(uint8_t*)(s2-1);
+        }
+    }
+    return 0;
+}
+static void
+_handle_command(int argc, char * argv[], void * ctx){
+    /*
+     * stub code
+     */
+    int i;
+    PRINTS("Received Args:\r\n");
+    for(i = 0; i < argc; i++){
+        PRINTS(argv[i]);
+        PRINTS("\r\n");
+    }
+    //echo command
+    PRINTS("Handle Command: \r\n");
+    if(argc > 1 && _strncmp(argv[0], "echo", strlen("echo")) == 0){
+        PRINTS(argv[1]);
+    }
+}
+
+MSG_CliUserListener_t *  Cli_User_Init(MSG_Central_t * parent, void * ctx){
+    self.listener.handle_command = _handle_command;
+    self.parent = parent;
+    return &self.listener;
+}
