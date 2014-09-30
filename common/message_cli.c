c58c0269d3f3800ff66c387661847cac27238287
Jackson Chen <jchen@hello.is>
2014-09-29 17:10:48 -0700

    Added user command parser

--- common/message_cli.c
+++ common/message_cli.c
@@ -1,3 +1,93 @@
 #include <stddef.h>
+#include <string.h>
 #include "util.h"
 #include "message_cli.h"
+static struct{
+    MSG_Base_t base;
+    MSG_Central_t * parent;
+    MSG_CliUserListener_t * user;
+}self;
+static char * name = "CLI";
+
+static MSG_Status
+_init(void){
+    return SUCCESS;
+}
+
+static MSG_Status
+_destroy(void){
+    return SUCCESS;
+}
+
+static MSG_Status
+_flush(void){
+    return SUCCESS;
+}
+static bool
+_isspace(char b){
+    return b == ' ';
+}
+static char *
+_next_token(char ** str){
+    char * ret;
+    char *ptr = *str;
+
+    while( _isspace(*ptr)){
+        ++ptr;
+    }
+    if(! *ptr){
+        return NULL;
+    }
+    ret = (char *)ptr;
+
+    while(*ptr && !_isspace(*ptr)){
+        ++ptr;
+    }
+    *str = ptr;
+    return ret;
+}
+
+static int
+_tokenize(char * string, char **argv){
+    int argc = 0;
+    char * token;
+    while(*string != '\0'){
+        argv[argc++] = (token = _next_token(&string));
+        if(*string){
+            *string++ = '\0';
+        }
+    }
+    return argc;
+}
+
+static MSG_Status
+_handle_raw_command(MSG_Address_t src, MSG_Address_t dst, MSG_Data_t * data){
+    char * argv[CLI_MAX_ARGS] = {0};
+    int argc = _tokenize(data->buf,argv);
+    int i;
+    /*
+     *PRINTS("args: \r\n");
+     *for(i = 0; i < argc; i++){
+     *    PRINTS(argv[i]);
+     *    PRINTS("\r\n");
+     *}
+     */
+    if(self.user){
+        self.handle_command(argc, argv);
+    }
+    return SUCCESS;
+
+}
+
+
+MSG_Base_t * MSG_Cli_Base(const MSG_Central_t * parent, const MSG_CliUserListener_t * user){
+    self.base.init = _init;
+    self.base.destroy = _destroy;
+    self.base.flush = _flush;
+    self.base.send = _handle_raw_command;
+    self.base.type = CLI;
+    self.base.typestr = name;
+    self.parent = parent;
+    self.user = user;
+    return &self.base;
+}
--- common/message_cli.h
+++ common/message_cli.h
@@ -1,7 +1,11 @@
 #pragma once
 #include "message_base.h"
 
-#define MSG_CLI_MAX_COMMANDS 5
-#define MSG_CLI_MAX_NAME_LEN 8
+#define CLI_MAX_ARGS 3
 
+typedef struct{
+    void (*handle_command)(int argc, char * argv[]);
+}MSG_CliUserListener_t;
+
+MSG_Base_t * MSG_Cli_Base(const MSG_Central_t * parent, const MSG_CliUserListener_t * user);
 
