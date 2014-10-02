0e47775a02ac83025fac550e628800ffb81b81a8
Pang Wu <pang@sayhello.com>
2014-10-02 15:55:40 -0700

    more orgainzed

--- pill/led.c
+++ pill/led.c
@@ -83,6 +83,22 @@ void led_power_off()
     nrf_gpio_pin_set(LED1_ENABLE);  // write 0
     */
 
+    /*
+    nrf_gpio_cfg_output(VRGB_SELECT);
+    nrf_gpio_pin_clear(VRGB_SELECT);  // on: 1
+    */
+
+    /*
+    nrf_gpio_cfg_output(VRGB_ENABLE);
+    nrf_gpio_pin_clear(VRGB_ENABLE);
+    */
+
+    /*
+    nrf_gpio_cfg_output(VLED_VDD_EN);
+    nrf_gpio_pin_set(VLED_VDD_EN);
+    */
+
+
     gpio_cfg_s0s1_output_connect(LED3_ENABLE, 1);
     gpio_cfg_d0s1_output_disconnect(LED3_ENABLE);
     gpio_cfg_s0s1_output_connect(LED2_ENABLE, 1);
@@ -90,26 +106,17 @@ void led_power_off()
     gpio_cfg_s0s1_output_connect(LED1_ENABLE, 1);
     gpio_cfg_d0s1_output_disconnect(LED1_ENABLE);
 
-    /*
-    nrf_gpio_cfg_output(VRGB_SELECT);
-    nrf_gpio_pin_clear(VRGB_SELECT);  // on: 1
-    */
+    
     gpio_cfg_s0s1_output_connect(VRGB_SELECT, 0);
     gpio_cfg_d0s1_output_disconnect(VRGB_SELECT);
 
 
-    /*
-    nrf_gpio_cfg_output(VRGB_ENABLE);
-    nrf_gpio_pin_clear(VRGB_ENABLE);
-    */
+    
     gpio_cfg_s0s1_output_connect(VRGB_ENABLE, 0);
     gpio_cfg_d0s1_output_disconnect(VRGB_ENABLE);
 
 
-    /*
-    nrf_gpio_cfg_output(VLED_VDD_EN);
-    nrf_gpio_pin_set(VLED_VDD_EN);
-    */
+    
     gpio_cfg_s0s1_output_connect(VLED_VDD_EN, 1);
     gpio_cfg_d0s1_output_disconnect(VLED_VDD_EN);
 
