4c0d7706529490076af7161b0d8937c1e8527964
Pang Wu <pang@sayhello.com>
2014-10-04 17:46:29 -0700

    TRI state output pin implementation

--- drivers/gpio_nor.h
+++ drivers/gpio_nor.h
@@ -5,4 +5,4 @@
 void gpio_cfg_d0s1_output_disconnect(uint32_t pin_number);
 void gpio_cfg_s0s1_output_connect(uint32_t pin_number, uint8_t init_val);
 void gpio_input_disconnect(uint32_t pin_number);
-void gpio_output_disconnect2(uint32_t pin_number);
+void gpio_cfg_h0d1_opendrain_output_connect(uint32_t pin_number, uint8_t init_val, nrf_gpio_pin_pull_t pin_pull);
