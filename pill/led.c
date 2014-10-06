07f1d2fb380153e692efc563ca5f5b36cf18d903
Pang Wu <pang@sayhello.com>
2014-10-06 14:53:04 -0700

    The VDD disconnect has to be d0s1

--- pill/led.c
+++ pill/led.c
@@ -20,7 +20,7 @@ static volatile uint8_t _blink_count;
 void led_power_on()
 {
 #ifdef PLATFORM_HAS_VLED
-    gpio_cfg_d0s1_opendrain_output_connect(VLED_VDD_EN, 0, NRF_GPIO_PIN_PULLDOWN);  // on: 0  VDD on
+    gpio_cfg_s0s1_output_connect(VLED_VDD_EN, 0);  // on: 0  VDD on
     gpio_cfg_s0s1_output_connect(VRGB_ENABLE, 1);  // on: 1  Boost on
 
     // 1 + NRF_GPIO_PIN_PULLUP  = RED ONLY + NO PWM ADJUST <- VERY TINY RED
@@ -95,14 +95,14 @@ void led_power_off()
         // Boost off and disconnect
 
         gpio_cfg_s0s1_output_connect(VRGB_ENABLE, 0);
-        gpio_cfg_d0s1_output_disconnect(VRGB_ENABLE);  // on: 1  Boost 5mA leak
+        //gpio_cfg_d0s1_output_disconnect(VRGB_ENABLE);  // on: 1  Boost 5mA leak
     }
     nrf_delay_ms(100);
     
     {
         // VDD off and disconnect
-        //gpio_cfg_d0s1_opendrain_output_connect(VLED_VDD_EN, 1, NRF_GPIO_PIN_PULLDOWN);
-        gpio_cfg_d0s1_output_disconnect_pull(VLED_VDD_EN, NRF_GPIO_PIN_PULLDOWN);
+        gpio_cfg_s0s1_output_connect(VLED_VDD_EN, 1);
+        gpio_cfg_d0s1_output_disconnect(VLED_VDD_EN);  // This has to be D0S1 becasue 0 is on, and we don't want 0 after disconnect
     }
 
 #endif    
