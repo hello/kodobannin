e65f77d3aa2b6ad67c4bc0d72d4e40d55c25371a
Jackson Chen <jchen@hello.is>
2014-10-09 17:58:42 -0700

    Upload latest

--- tools/upload_hex.py
+++ tools/upload_hex.py
@@ -8,6 +8,7 @@ import sys
 S3_ACCESS_ID = "AKIAJ7SHF3VSR7KB7VMA"
 S3_SECRET_KEY = "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"
 BUILD_BASE_KEY = "builds/"
+BUILD_LATEST_KEY = "latest/"
 S3_ROOT = "hello-firmware"
 
 conn = S3Connection(S3_ACCESS_ID, S3_SECRET_KEY)
@@ -33,6 +34,8 @@ def upload(commit_info = "limbo"):
         for h in hexes:
             k = Key(bucket)
             k.key = os.path.join(BUILD_BASE_KEY, commit_info, extract_name(h))
+            k.set_contents_from_filename(h, cb = percent_cb, num_cb=10)
+            k.key = os.path.join(BUILD_LATEST_KEY, commit_info, extract_name(h))
             k.set_contents_from_filename(h, cb = percent_cb, num_cb=10)
         print "Uploads finished"
         return True
