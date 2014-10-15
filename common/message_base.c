8b3f547842c6937d978501b71a6ab65aac6821e9
Jackson Chen <jchen@hello.is>
2014-10-15 15:56:35 -0700

    clear list

--- common/message_base.c
+++ common/message_base.c
@@ -203,6 +203,7 @@ MSG_Queue_t * MSG_Base_InitQueue(void * mem, uint32_t size){
         //minimum of one element is required
         ret = NULL;
     }else{
+        memset(mem, 0, size);
         ret = (MSG_Queue_t *)mem;
         ret->capacity = (size - sizeof(MSG_Queue_t)) / sizeof(MSG_Data_t *);
         ret->elements = 0;
