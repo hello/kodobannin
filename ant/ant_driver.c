a51b8c57634187cf6d53c97cb8e46aa6e13c2029
Jackson Chen <jchen@hello.is>
2014-11-05 17:41:32 -0800

    crank up power

--- ant/ant_driver.c
+++ ant/ant_driver.c
@@ -60,7 +60,7 @@ _configure_channel(uint8_t channel,const hlo_ant_channel_phy_t * phy,  const hlo
     ret += sd_ant_channel_id_set(channel, device->device_number, device->device_type, device->transmit_type);
     ret += sd_ant_channel_low_priority_rx_search_timeout_set(channel, 0xFF);
     ret += sd_ant_channel_rx_search_timeout_set(channel, 0);
-    ret += sd_ant_channel_radio_tx_power_set(channel, RADIO_TX_POWER_LVL_3, 0);
+    ret += sd_ant_channel_radio_tx_power_set(channel, RADIO_TX_POWER_LVL_4, 0);
     return ret;
 }
 static int
@@ -111,6 +111,8 @@ int32_t hlo_ant_connect(const hlo_ant_device_t * device){
     int ch = _find_open_channel_by_device(device, begin,7);
     if(ch >= begin){
         PRINTS("ANT: Channel already open!\r\n");
+        uint8_t message[8] = {0};
+        sd_ant_broadcast_message_tx((uint8_t)ch, sizeof(message), message);
         return 0;
     }else{
         //open channel
