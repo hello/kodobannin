95c87b5c4d26f922c6079fc2f2a0d158d54c22d0
Jackson Chen <jchen@hello.is>
2014-11-21 17:42:47 -0800

    complete provisioning

--- bootloader/factory_provision.c
+++ bootloader/factory_provision.c
@@ -9,16 +9,16 @@
 static pstorage_handle_t        __attribute__((aligned(4))) m_info_handle;
 static device_info_t __attribute__((aligned(4))) test_info;
 static volatile uint32_t status;
+volatile uint32_t code;
 
 static void
 _pstorage_cb(pstorage_handle_t * handle, uint8_t op_code, uint32_t result, uint8_t * p_data, uint32_t data_len){
     APP_ERROR_CHECK(result);
     switch(op_code){
         case PSTORAGE_CLEAR_OP_CODE:
-            /*SIMPRINTS("Clear");*/
             break;
         case PSTORAGE_STORE_OP_CODE:
-            /*SIMPRINTS("Store");*/
+            status = 0;
             break;
         default:
             /*SIMPRINTS("Unknown Opcode");*/
@@ -30,8 +30,11 @@ _pstorage_cb(pstorage_handle_t * handle, uint8_t op_code, uint32_t result, uint8
 static void wait_for_events(void){
     for (;;)
     {
-        // Event received. Process it from the scheduler.
+        //handle the event that is generated before this
         app_sched_execute();
+        if(!status){
+            return;
+        }
         // Wait in low power state for any events.
         uint32_t err_code = sd_app_evt_wait();
         APP_ERROR_CHECK(err_code);
@@ -44,22 +47,13 @@ bool factory_needs_provisioning(void){
 void _gen_key(void * data, uint16_t size){
     uint32_t err_code;
     generate_new_device(&test_info);
-    SIMPRINTS("Device Generated: \r\n");
-    err_code = pstorage_raw_clear(&m_info_handle, sizeof(test_info));
-    if(err_code != NRF_SUCCESS){
-        goto fail;
-    }
 
-    err_code = pstorage_raw_store(&m_info_handle, 
-                              (uint8_t *)&test_info, 
-                              sizeof(test_info),
-                              0);
-    if(err_code != NRF_SUCCESS){
-        goto fail;
-    }
-fail:
-    SIMPRINTS("Error == ");
-    SIMPRINT_HEX(&err_code, 2);
+    pstorage_raw_clear(&m_info_handle, sizeof(test_info));
+
+    pstorage_raw_store(&m_info_handle, 
+            (uint8_t *)&test_info, 
+            sizeof(test_info),
+            0);
 }
 
 uint32_t factory_provision_start(void){
