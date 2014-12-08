f7a42c1b31b50f21be1bb13cf1e1a1e325610237
Jackson Chen <jchen@hello.is>
2014-12-08 11:28:09 +0800

    Remove dependency for debug in print

--- common/message_uart.h
+++ common/message_uart.h
@@ -11,8 +11,10 @@
 #define MSG_UART_COMMAND_MAX_SIZE 32
 
 MSG_Base_t * MSG_Uart_Base(const app_uart_comm_params_t * params, const MSG_Central_t * parent);
-//quick print function for debug
+
+//function for async print
 void MSG_Uart_Prints(const char * str);
+void MSG_Uart_Printc(char c);
 void MSG_Uart_PrintHex(const uint8_t * ptr, uint32_t len);
 
 
