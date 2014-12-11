4e268e6efbff3a7683827bee50d34a21f34bf9a7
Jackson Chen <jchen@hello.is>
2014-12-11 16:31:48 +0800

    remove print

--- pill/led_booster_timer.c
+++ pill/led_booster_timer.c
@@ -95,7 +95,6 @@ uint8_t led_booster_is_free(void){
 void led_booster_power_on(void){
     uint32_t ticks = APP_TIMER_TICKS(1000, APP_TIMER_PRESCALER);
     if(led_check_reedswitch() == 1){
-        PRINTS("RS Low\r\n");
         return;
     }
     CRITICAL_REGION_ENTER();
