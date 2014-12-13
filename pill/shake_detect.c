df3c338004f29c97ff000cb78a2f458cc90c51e9
Pang Wu <pang@sayhello.com>
2014-12-12 17:56:28 -0800

    Do not detect shake for 3 seconds after the previous shake is detected

--- pill/shake_detect.c
+++ pill/shake_detect.c
@@ -1,10 +1,12 @@
 #include "app.h"
 #include "shake_detect.h"
 #include "util.h"
+#include "timedfifo.h"
 
 static struct{
     uint32_t thmag, cnt;
     uint8_t shaking_time_sec;
+    uint8_t last_shaking_time_sec;
     shake_detection_callback_t _shake_detection_callback;
 }self;
 
@@ -27,6 +29,7 @@ void ShakeDetectDecWindow(void){
     if(self.shaking_time_sec > SLIDING_WINDOW_SIZE_SEC){
         // sliding window ends, reset all the counter
         if(self.cnt >= SHAKING_DATA_COUNT_THRESHOLD){ // The user shakes hard enough and long enough
+            self.last_shaking_time_sec = get_tick();
             if(self._shake_detection_callback){
                 self._shake_detection_callback();
             }
@@ -37,6 +40,13 @@ void ShakeDetectDecWindow(void){
 }
 
 bool ShakeDetect(uint32_t accelmag){
+    uint8_t current_time_sec = get_tick();
+    // Do not detect shaking for 3 seconds after the previous shake is detected.
+    if(current_time_sec - self.last_shaking_time_sec < MIN_SHAKE_DETECT_INTERVAL_SEC)
+    {
+        return false;
+    }
+
     if(accelmag >= self.thmag){
         if(!self.shaking_time_sec){
             ++self.shaking_time_sec;  // initialize the sliding window timer.
