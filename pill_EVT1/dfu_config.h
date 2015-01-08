4c15c7033f8eded3dfa4c725e247d17ce176c188
Jackson Chen <jchen@hello.is>
2015-01-07 16:44:44 -0800

    removed EVT

--- pill_EVT1/dfu_config.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-/**
- * contains platform specfici DFU settings for bootloader
- * Reason being bootloader is a separate app differentiated only by platform for pill/morpheus
- **/
-
-#define DEVICE_NAME                          "PillDFU"                                               /**< Name of device. Will be included in the advertising data. */
-#define MANUFACTURER_NAME                    "Hello Inc"                                   /**< Manufacturer. Will be passed to Device Information Service. */
