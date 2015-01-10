51be0428e09a328500fb7cfae28e3794ff71ad23
Pang Wu <pang@sayhello.com>
2015-01-10 00:25:49 -0800

    Bump up the ant data version so the backend can know it is long data

--- pill/app.h
+++ pill/app.h
@@ -22,7 +22,7 @@ enum {
 #define BLE_MANUFACTURER_ID   0x43110
 #define BLE_ORG_UNIQUE_ID     0x1337
 
-#define ANT_PROTOCOL_VER      (1)
+#define ANT_PROTOCOL_VER      (2)
 #define FIRMWARE_VERSION_8BIT (2)
 
 //#define BONDING_REQUIRED
