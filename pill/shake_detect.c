12b036aa56b258d6f3e5631543d3fa311ec627d8
Jackson Chen <jchen@hello.is>
2015-01-13 11:34:12 -0800

    do not merge 3s block for shake

--- pill/shake_detect.c
+++ pill/shake_detect.c
@@ -6,7 +6,6 @@
 static struct{
     uint32_t thmag, cnt;
     uint8_t shaking_time_sec;
-    uint8_t last_shaking_time_sec;
     shake_detection_callback_t _shake_detection_callback;
 }self;
 
@@ -40,18 +39,6 @@ void ShakeDetectDecWindow(void){
 }
 
 bool ShakeDetect(uint32_t accelmag){
-    uint8_t current_time_sec = get_tick();
-    int8_t diff = current_time_sec - self.last_shaking_time_sec;
-    if(diff < 0)
-    {
-        diff = MOTION_DATA_INTERVAL_SEC - self.last_shaking_time_sec + current_time_sec;
-    }
-    // Do not detect shaking for 3 seconds after the previous shake is detected.
-    if(current_time_sec - self.last_shaking_time_sec < MIN_SHAKE_DETECT_INTERVAL_SEC)
-    {
-        return false;
-    }
-
     if(accelmag >= self.thmag){
         if(!self.shaking_time_sec){
             ++self.shaking_time_sec;  // initialize the sliding window timer.
