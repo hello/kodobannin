4e32fc0ce5b10abddcb4ebf60a8948fb84866580
Jackson Chen <jchen@hello.is>
2015-01-24 11:57:47 +0800

    remove test code calls

--- pill/cli_user.c
+++ pill/cli_user.c
@@ -125,41 +125,6 @@ _handle_command(int argc, char * argv[]){
                                 (MSG_Address_t){IMU, IMU_SELF_TEST},
                                 NULL);
     }
-    if( !match_command(argv[0], "imuon") ){
-        self.parent->loadmod(MSG_IMU_GetBase());
-    }
-#ifdef PLATFORM_HAS_VLED
-    if( !match_command(argv[0], "bat") ){
-        test_bat();
-    }
-    if( !match_command(argv[0], "red") ){
-        test_red();
-    }
-    if( !match_command(argv[0], "grn") ){
-        test_grn();
-    }
-    if( !match_command(argv[0], "blu") ){
-        test_blu();
-    }
-    if( !match_command(argv[0], "rgb") ){
-        test_rgb();
-    }
-    if( !match_command(argv[0], "led") ){
-        test_led();
-    }
-    if( !match_command(argv[0], "dsp") ){
-        led_update_battery_status();
-    }
-#endif
-    if( !match_command(argv[0], "mon") ){
-        cli_update_battery_level(0); // monitor droop
-    }
-    if( !match_command(argv[0], "upd") ){
-        cli_update_battery_level(1); // measure battery / issue ant packet
-    }
-    if( !match_command(argv[0], "advstop")){
-        sd_ble_gap_adv_stop();
-    }
     //dispatch message through ANT
     if(argc > 1 && !match_command(argv[0], "ant") ){
         //Create a message object from uart string
