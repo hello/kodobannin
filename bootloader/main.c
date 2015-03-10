20b5a4d8c80b4b3257e868d770d875699bff924a
Jackson Chen <jchen@hello.is>
2015-03-10 14:35:42 -0700

    remove unused code

--- bootloader/main.c
+++ bootloader/main.c
@@ -29,6 +29,7 @@
 #include "util.h"
 #include "git_description.h"
 #include "factory_provision.h"
+#include "gpio_nor.h"
 
 
 enum {
@@ -39,55 +40,6 @@ enum {
     SCHED_MAX_EVENT_DATA_SIZE = MAX(APP_TIMER_SCHED_EVT_SIZE, 0),
     SCHED_QUEUE_SIZE = 20,
 };
-
-static void
-_sha1_fw_area(uint8_t *hash)
-{
-
-	uint32_t code_size    = DFU_IMAGE_MAX_SIZE_FULL;
-	uint8_t *code_address = (uint8_t *)CODE_REGION_1_START;
-	uint32_t *index = (uint32_t *)BOOTLOADER_REGION_START - DFU_APP_DATA_RESERVED;
-
-	// walk back to the end of the actual firmware
-	while (index > CODE_REGION_1_START && *--index == EMPTY_FLASH_MASK && code_size > 0)
-		code_size -= 4;
-
-    // only measure if there is something to measure
-    memset(hash, 0, SHA1_DIGEST_LENGTH);
-    if (code_size ==  0)
-        return;
-
-	NRF_RTC1->TASKS_START = 1;
-
-	uint32_t start_ticks, stop_ticks;
-
-	start_ticks = NRF_RTC1->COUNTER;
-    sha1_calc(code_address, code_size, hash);
-	stop_ticks = NRF_RTC1->COUNTER;
-
-	debug_print_ticks("SHA1 time in ticks: ", start_ticks, stop_ticks);
-
-	NRF_RTC1->TASKS_STOP = 1;
-}
-
-static bool
-_verify_fw_sha1(uint8_t *valid_hash)
-{
-	uint8_t sha1[SHA1_DIGEST_LENGTH];
-    uint8_t comp = 0;
-    int i = 0;
-
-    _sha1_fw_area(sha1);
-
-#ifdef DEBUG
-    DEBUG("SHA1: ", sha1);
-#endif
-
-    for (i = 0; i < SHA1_DIGEST_LENGTH; i++)
-        comp |= sha1[i] ^ valid_hash[i];
-
-    return comp == 0;
-}
 
 bool dfu_success = false;
 
@@ -151,13 +103,15 @@ _start()
 
 		ret = factory_provision_start();
 
-#ifdef DEBUG_SERIAL
 		if(ret == NRF_SUCCESS){
+#ifdef DEBUG_SERIAL
 			SIMPRINTS("Successful\r\n");
-		}else{
-			SIMPRINTS("Failed\r\n");
-		}
 #endif
+		}else{
+#ifdef DEBUG_SERIAL
+			SIMPRINTS("Failed\r\n");
+#endif
+		}
 		NVIC_SystemReset();
 	}
 
