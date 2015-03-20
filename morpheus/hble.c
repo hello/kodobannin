91297815dd95a393174b7c032cca498f1285f6b3
Jackson Chen <jchen@hello.is>
2015-03-19 19:30:31 -0700

    disallow outsider to call task queue

--- morpheus/hble.c
+++ morpheus/hble.c
@@ -864,9 +864,6 @@ void hble_services_init(void)
     
     
 }
-int32_t hble_task_queue(delay_task_t t){
-	return _task_queue(t);
-}
 void hble_refresh_bonds(){
 	if(hlo_ble_is_connected){
 		//disconnect
--- morpheus/hble.h
+++ morpheus/hble.h
@@ -22,8 +22,6 @@ bool hble_uint64_to_hex_device_id(uint64_t device_id, char* hex_device_id, size_
 bool hble_hex_to_uint64_device_id(const char* hex_device_id, uint64_t* device_id);
 uint64_t hble_get_device_id();
 
-int32_t hble_task_queue(delay_task_t t);
-
 void hble_set_advertise_callback(on_advertise_started_callback_t cb);
 void hble_set_bond_status_callback(bond_status_callback_t callback);
 void hble_set_connected_callback(connected_callback_t callback);
