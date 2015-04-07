5978c532c702c822bcb765b5f7dda134c8b53654
Jackson Chen <jchen@hello.is>
2015-04-07 16:58:36 -0700

    update version to 0.6.2

--- common/app_info.h
+++ common/app_info.h
@@ -3,7 +3,7 @@
  * A place to put all common defines
  */
 
-#define FW_VERSION_STRING "0.6.1"
+#define FW_VERSION_STRING "0.6.2"
 
-#define FIRMWARE_VERSION_8BIT (6)
+#define FIRMWARE_VERSION_8BIT (7)
 
