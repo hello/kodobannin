49cda9b098596cb17a0fd8193ba0111996d864bd
Jackson Chen <jchen@hello.is>
2015-05-27 17:56:14 -0700

    send version

--- common/app_info.h
+++ common/app_info.h
@@ -2,7 +2,7 @@
 /* 
  * A place to put all common defines
  */
-
+//make sure the FW_VERSION_STRING fits in MorpheusCommand.top_version (16 bytes)
 #define FW_VERSION_STRING "0.7.0"
 
 #define FIRMWARE_VERSION_8BIT (16)
