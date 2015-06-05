66104d02695e627ce06571062acf30797c432ab7
Jackson Chen <jchen@hello.is>
2015-06-05 14:59:57 -0700

    update port number

--- common/message_sspi.h
+++ common/message_sspi.h
@@ -4,4 +4,10 @@
 #include "message_base.h"
 
 
+typedef enum{
+    MSG_SSPI_DEFAULT = 0,
+    MSG_SSPI_MORPHEUS_BLE_PROTO = 1,
+    MSG_SSPI_TEXT,
+}MSG_SSPI_Ports;
+
 MSG_Base_t * MSG_SSPI_Base(const spi_slave_config_t * p_spi_slave_config, const MSG_Central_t * parent);
