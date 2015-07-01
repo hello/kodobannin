185b0a0febd7c94e1af9189925cd4ebd90019d78
Jackson Chen <jchen@hello.is>
2015-07-01 13:46:00 -0700

    update version string to 0.8.1

--- common/app_info.h
+++ common/app_info.h
@@ -3,7 +3,8 @@
  * A place to put all common defines
  */
 //make sure the FW_VERSION_STRING fits in MorpheusCommand.top_version (16 bytes)
-#define FW_VERSION_STRING "0.8.0"
+#define FW_VERSION_STRING "0.8.1"
 
+//pill only
 #define FIRMWARE_VERSION_8BIT (17)
 
