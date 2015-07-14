e77dd18363b35a3930e7ddd3d7a15750486a993e
plasticchris <plasticchris@gmail.com>
2015-07-14 13:53:06 -0700

    limit ant packet size

--- common/app_info.h
+++ common/app_info.h
@@ -3,7 +3,7 @@
  * A place to put all common defines
  */
 //make sure the FW_VERSION_STRING fits in MorpheusCommand.top_version (16 bytes)
-#define FW_VERSION_STRING "0.7.5"
+#define FW_VERSION_STRING "0.7.6"
 
 #define FIRMWARE_VERSION_8BIT (16)
 
