6f907b8e8cd117d971be79caa1f7aa05ef63c7f6
plasticchris <plasticchris@gmail.com>
2015-08-05 13:28:33 -0700

    make the uptime in seconds accurate

--- pill/app.h
+++ pill/app.h
@@ -112,10 +112,8 @@ enum {
 #define BATTERY_LEVEL_MEAS_INTERVAL          APP_TIMER_TICKS(200000, APP_TIMER_PRESCALER)
 #define IMU_ACTIVE_INTERVAL                  APP_TIMER_TICKS(500, APP_TIMER_PRESCALER)
 #define LED_INIT_LIGHTUP_INTERAVL            APP_TIMER_TICKS(200,APP_TIMER_PRESCALER)
-#define HEARTBEAT_INTERVAL_SEC              (3600/60)
-#define BATTERY_INTERVAL_SEC                (600/60)
-#define MOTION_DATA_INTERVAL_SEC            (60/60)
-#define BATT_MEASURE_INTERVAL_SEC           (1200/60)  /* Measure battery level every 20 minutes */
+#define HEARTBEAT_INTERVAL_MIN              (3600/60)
+#define BATT_MEASURE_INTERVAL_MIN           (1200/60)  /* Measure battery level every 20 minutes */
 
 #define SHAKING_MOTION_THRESHOLD        (750000000)
 #define SLIDING_WINDOW_SIZE_SEC         (2) // shake second timer runs on imu active timer at 2hz now, so this is 1/2 secs
