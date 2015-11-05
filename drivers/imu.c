50cf61576478e248d66eaea59d4708ee3bddcf48
Divya Kamath <divkam1987@gmail.com>
2015-11-05 15:35:54 -0800

    DEBUG flag reset, indentation fixed
    - changes made based on PR review

--- drivers/imu.c
+++ drivers/imu.c
@@ -20,7 +20,7 @@
 
 
 enum {
-    IMU_COLLECTION_INTERVAL = 6553, // in timer ticks, so 200ms (0.2*32768)
+	IMU_COLLECTION_INTERVAL = 6553, // in timer ticks, so 200ms (0.2*32768)
 };
 
 static SPI_Context _spi_context;
@@ -45,60 +45,60 @@ static inline void _register_write(Register_t register_address, uint8_t value)
 
 inline void imu_set_accel_freq(enum imu_hz sampling_rate)
 {
-    uint8_t reg;
-    _register_read(REG_CTRL_1, &reg);
+	uint8_t reg;
+	_register_read(REG_CTRL_1, &reg);
 
-    // Clear the ODR
-    reg &= ~OUTPUT_DATA_RATE;
-    
-    reg |= ( sampling_rate << 4);
-    _register_write(REG_CTRL_1, reg);
+	// Clear the ODR
+	reg &= ~OUTPUT_DATA_RATE;
+
+	reg |= ( sampling_rate << 4);
+	_register_write(REG_CTRL_1, reg);
 
 }
 
 unsigned imu_get_sampling_interval(enum imu_hz hz)
 {
-    switch (hz) {
-        case IMU_HZ_0:
-        return 0;
-        break;
-        case IMU_HZ_10:
-        return 10;
-        case IMU_HZ_25:
-        return 25;
-        
-        default:
-        return 0;
-    }
+	switch (hz) {
+	case IMU_HZ_0:
+		return 0;
+		break;
+	case IMU_HZ_10:
+		return 10;
+	case IMU_HZ_25:
+		return 25;
+
+	default:
+		return 0;
+	}
 }
 
 uint16_t imu_accel_reg_read(uint16_t *values) {
-    uint8_t * buf = (uint8_t*)values;
+	uint8_t * buf = (uint8_t*)values;
 	_register_read(REG_ACC_X_LO, buf++);
 	_register_read(REG_ACC_X_HI, buf++);
 	_register_read(REG_ACC_Y_LO, buf++);
 	_register_read(REG_ACC_Y_HI, buf++);
 	_register_read(REG_ACC_Z_LO, buf++);
 	_register_read(REG_ACC_Z_HI, buf++);
-    
 
-    uint8_t reg;
-    _register_read(REG_CTRL_4, &reg);
-    if( reg & HIGHRES ) { //convert to match the 6500...
-        values[0] *= 16;
-        values[1] *= 16;
-        values[2] *= 16;
-    } else {
-        values[0] *= 262;
-        values[1] *= 262;
-        values[2] *= 262;
-    }
 
-    /*
+	uint8_t reg;
+	_register_read(REG_CTRL_4, &reg);
+	if( reg & HIGHRES ) { //convert to match the 6500...
+		values[0] *= 16;
+		values[1] *= 16;
+		values[2] *= 16;
+	} else {
+		values[0] *= 262;
+		values[1] *= 262;
+		values[2] *= 262;
+	}
+
+	/*
 	DEBUG("The value[0] is 0x",values[0]);
 	DEBUG("The value[1] is 0x",values[1]);
 	DEBUG("The value[2] is 0x",values[2]);
-     */
+	 */
 
 	return 6;
 }
@@ -109,10 +109,10 @@ inline void imu_set_accel_range(enum imu_accel_range range)
 
 	_register_read(REG_CTRL_4, &reg);
 
-    // Clear the FSR
-    reg &= ~FS_MASK;
+	// Clear the FSR
+	reg &= ~FS_MASK;
 
-    _register_write(REG_CTRL_4, reg | (range << ACCEL_CFG_SCALE_OFFSET));
+	_register_write(REG_CTRL_4, reg | (range << ACCEL_CFG_SCALE_OFFSET));
 }
 
 inline void imu_enable_all_axis()
@@ -120,66 +120,66 @@ inline void imu_enable_all_axis()
 	uint8_t reg;
 
 	_register_read(REG_CTRL_1, &reg);
-    _register_write(REG_CTRL_1, reg | (AXIS_ENABLE));
+	_register_write(REG_CTRL_1, reg | (AXIS_ENABLE));
 }
 
 inline uint8_t imu_clear_interrupt_status()
 {
-    // clear the interrupt by reading INT_SRC register
-    uint8_t int_source;
-    _register_read(REG_INT1_SRC, &int_source);
-    DEBUG("The INT1_SRC is 0x",int_source);
+	// clear the interrupt by reading INT_SRC register
+	uint8_t int_source;
+	_register_read(REG_INT1_SRC, &int_source);
+	DEBUG("The INT1_SRC is 0x",int_source);
 
 
-    return int_source;
+	return int_source;
 }
 
 inline uint8_t imu_clear_interrupt2_status()
 {
-    // clear the interrupt by reading INT_SRC register
-    uint8_t int_source;
+	// clear the interrupt by reading INT_SRC register
+	uint8_t int_source;
 
-    _register_read(REG_INT2_SRC, &int_source);
+	_register_read(REG_INT2_SRC, &int_source);
 
-    return int_source;
+	return int_source;
 }
 
 void imu_enter_normal_mode()
 {
-    uint8_t reg;
-    _register_read(REG_CTRL_1, &reg);
-    reg &= ~LOW_POWER_MODE;
-    _register_write(REG_CTRL_1, reg);
-    
+	uint8_t reg;
+	_register_read(REG_CTRL_1, &reg);
+	reg &= ~LOW_POWER_MODE;
+	_register_write(REG_CTRL_1, reg);
+
 #ifdef IMU_ENABLE_HRES
 
-    _register_read(REG_CTRL_4, &reg);
-    _register_write(REG_CTRL_4, reg | HIGHRES );
+	_register_read(REG_CTRL_4, &reg);
+	_register_write(REG_CTRL_4, reg | HIGHRES );
 
 #endif
 }
 
 void imu_enter_low_power_mode()
 {
-    uint8_t reg;
+	uint8_t reg;
 
 #ifdef IMU_ENABLE_HRES
 
-    _register_read(REG_CTRL_4, &reg);
-    reg &= ~HIGHRES;
-    _register_write(REG_CTRL_4, reg);
+	_register_read(REG_CTRL_4, &reg);
+	reg &= ~HIGHRES;
+	_register_write(REG_CTRL_4, reg);
 
 #endif
 
 
-    _register_read(REG_CTRL_1, &reg);
-    _register_write(REG_CTRL_1, reg | LOW_POWER_MODE);
+	_register_read(REG_CTRL_1, &reg);
+	_register_write(REG_CTRL_1, reg | LOW_POWER_MODE);
 }
 
 
 void imu_wom_set_threshold(uint16_t microgravities)
 {
-    _register_write(REG_INT1_THR, microgravities / 16);
+	_register_write(REG_INT1_THR, microgravities / 16);
 }
 
 
@@ -203,26 +203,26 @@ void imu_spi_disable()
 inline void imu_power_on()
 {
 #ifdef PLATFORM_HAS_IMU_VDD_CONTROL
-    gpio_cfg_s0s1_output_connect(IMU_VDD_EN, 0);
+	gpio_cfg_s0s1_output_connect(IMU_VDD_EN, 0);
 #endif
 }
 
 inline void imu_power_off()
 {
 #ifdef PLATFORM_HAS_IMU_VDD_CONTROL
-    gpio_cfg_s0s1_output_connect(IMU_VDD_EN, 1);
-    gpio_cfg_d0s1_output_disconnect(IMU_VDD_EN);
+	gpio_cfg_s0s1_output_connect(IMU_VDD_EN, 1);
+	gpio_cfg_d0s1_output_disconnect(IMU_VDD_EN);
 
 #endif
 }
 
 int32_t imu_init_low_power(enum SPI_Channel channel, enum SPI_Mode mode, 
-			uint8_t miso, uint8_t mosi, uint8_t sclk, 
-			uint8_t nCS, 
-			enum imu_hz sampling_rate,
-			enum imu_accel_range acc_range, uint16_t wom_threshold)
+		uint8_t miso, uint8_t mosi, uint8_t sclk,
+		uint8_t nCS,
+		enum imu_hz sampling_rate,
+		enum imu_accel_range acc_range, uint16_t wom_threshold)
 {
- 	int32_t err;
+	int32_t err;
 
 	err = spi_init(channel, mode, miso, mosi, sclk, nCS, &_spi_context);
 	if (err != 0) {
@@ -246,8 +246,8 @@ int32_t imu_init_low_power(enum SPI_Channel channel, enum SPI_Mode mode,
 
 	_register_write(REG_CTRL_1, AXIS_ENABLE);
 
-    // Set inactive sampling rate (Ctrl Reg 1)
-    imu_set_accel_freq(sampling_rate);
+	// Set inactive sampling rate (Ctrl Reg 1)
+	imu_set_accel_freq(sampling_rate);
 
 	_register_write(REG_CTRL_2, HIGHPASS_AOI_INT1);
 
@@ -258,44 +258,44 @@ int32_t imu_init_low_power(enum SPI_Channel channel, enum SPI_Mode mode,
 	_register_write(REG_CTRL_4, BLOCKDATA_UPDATE);//80
 
 	// Set full scale range (Ctrl 4)
-    imu_set_accel_range(acc_range);
+	imu_set_accel_range(acc_range);
 
-    // Interrupt request latched
-    _register_write(REG_CTRL_5, (LATCH_INTERRUPT1));
+	// Interrupt request latched
+	_register_write(REG_CTRL_5, (LATCH_INTERRUPT1));
 
 
-    uint8_t reg;
+	uint8_t reg;
 
-    // reset HP filter
-    _register_read(REG_REFERENCE,&reg);
+	// reset HP filter
+	_register_read(REG_REFERENCE,&reg);
 
-    // Enable OR combination interrupt generation for all axis
-    reg = INT1_Z_HIGH | INT1_Z_LOW | INT1_Y_HIGH | INT1_Y_LOW | INT1_X_HIGH | INT1_X_LOW;
-    _register_write(REG_INT1_CFG, reg | INT1_6D); //all axis
+	// Enable OR combination interrupt generation for all axis
+	reg = INT1_Z_HIGH | INT1_Z_LOW | INT1_Y_HIGH | INT1_Y_LOW | INT1_X_HIGH | INT1_X_LOW;
+	_register_write(REG_INT1_CFG, reg | INT1_6D); //all axis
 
-    imu_wom_set_threshold(wom_threshold);
+	imu_wom_set_threshold(wom_threshold);
 
-    // TODO
-    //_register_write(REG_ACT_THR,wom_threshold);
+	// TODO
+	//_register_write(REG_ACT_THR,wom_threshold);
 
-    int16_t values[3];
-    imu_accel_reg_read(values);
-    (void) values;
+	int16_t values[3];
+	imu_accel_reg_read(values);
+	(void) values;
 
-    imu_clear_interrupt_status();
+	imu_clear_interrupt_status();
 
-    imu_enter_low_power_mode();
+	imu_enter_low_power_mode();
 
-    // Enable INT 1 function on INT 2 pin
-    _register_write(REG_CTRL_6, INT1_OUTPUT_ON_LINE_2);
+	// Enable INT 1 function on INT 2 pin
+	_register_write(REG_CTRL_6, INT1_OUTPUT_ON_LINE_2);
 
 	return err;
 }
 
-
+// TODO self test not implemented
 int imu_self_test(void){
-    
-	if(true){
+
+	if(false){
 
 		PRINTS("Pass\r\n");
 		return 0;
--- drivers/lis2dh_registers.h
+++ drivers/lis2dh_registers.h
@@ -7,127 +7,127 @@
 #define CHIP_ID 0x33
 
 typedef enum IMU_Registers {
-    REG_STATUS_AUX = 0x7,
-    REG_OUT_TEMP_LO = 0xc,
-    REG_OUT_TEMP_HI = 0xd,
-    REG_INT_COUNTER = 0xe,
-    REG_WHO_AM_I = 0xf,
-    REG_TEMP_CFG = 0x1f,
-    REG_CTRL_1 = 0x20,
-   	REG_CTRL_2 = 0x21,
-   	REG_CTRL_3 = 0x22,
-   	REG_CTRL_4 = 0x23,
-   	REG_CTRL_5 = 0x24,
-   	REG_CTRL_6 = 0x25,
-   	REG_REFERENCE = 0x26,
-   	REG_STATUS_2 = 0x27,
-    REG_ACC_X_HI = 0x28,
+	REG_STATUS_AUX = 0x7,
+	REG_OUT_TEMP_LO = 0xc,
+	REG_OUT_TEMP_HI = 0xd,
+	REG_INT_COUNTER = 0xe,
+	REG_WHO_AM_I = 0xf,
+	REG_TEMP_CFG = 0x1f,
+	REG_CTRL_1 = 0x20,
+	REG_CTRL_2 = 0x21,
+	REG_CTRL_3 = 0x22,
+	REG_CTRL_4 = 0x23,
+	REG_CTRL_5 = 0x24,
+	REG_CTRL_6 = 0x25,
+	REG_REFERENCE = 0x26,
+	REG_STATUS_2 = 0x27,
+	REG_ACC_X_HI = 0x28,
 	REG_ACC_X_LO = 0x29,
 	REG_ACC_Y_HI = 0x2a,
 	REG_ACC_Y_LO = 0x2b,
-    REG_ACC_Z_HI = 0x2c,
-    REG_ACC_Z_LO = 0x2d,
-    REG_FIFO_CTRL = 0x2e,
-    REG_FIFO_SRC = 0x2f,
-    REG_INT1_CFG = 0x30,
-    REG_INT1_SRC = 0x31,
-    REG_INT1_THR = 0x32,
-    REG_INT1_DUR = 0x33,
-    REG_INT2_CFG = 0x34,
-    REG_INT2_SRC = 0x35,
-    REG_INT2_THR = 0x36,
-    REG_INT2_DUR = 0x37,
-    REG_CLICK_CFG = 0x38,
-    REG_CLICK_SRC = 0x39,
-    REG_CLICK_THR = 0x3a,
-    REG_TIME_LIMIT = 0x3b,
-    REG_TIME_LATENCY = 0x3c,
-    REG_TIME_WINDOW = 0x3d,
-    REG_ACT_THR = 0x3e,
-    REG_ACT_DUR = 0x3f,
+	REG_ACC_Z_HI = 0x2c,
+	REG_ACC_Z_LO = 0x2d,
+	REG_FIFO_CTRL = 0x2e,
+	REG_FIFO_SRC = 0x2f,
+	REG_INT1_CFG = 0x30,
+	REG_INT1_SRC = 0x31,
+	REG_INT1_THR = 0x32,
+	REG_INT1_DUR = 0x33,
+	REG_INT2_CFG = 0x34,
+	REG_INT2_SRC = 0x35,
+	REG_INT2_THR = 0x36,
+	REG_INT2_DUR = 0x37,
+	REG_CLICK_CFG = 0x38,
+	REG_CLICK_SRC = 0x39,
+	REG_CLICK_THR = 0x3a,
+	REG_TIME_LIMIT = 0x3b,
+	REG_TIME_LATENCY = 0x3c,
+	REG_TIME_WINDOW = 0x3d,
+	REG_ACT_THR = 0x3e,
+	REG_ACT_DUR = 0x3f,
 
 } Register_t;
 
 #define ACCEL_CFG_SCALE_OFFSET 4
 
 enum IMU_Reg_Bits {
-    //REG_TEMP_CFG
+	//REG_TEMP_CFG
 	TEMPERATURE_DATA_OVERRUN  = 0x40,
-   	TEMPERATURE_DATA_AVAILABLE  = 0x4,
-    TEMPERATURE_ENABLE = 0xc0,
-    
-    //REG_CTRL_1
-    OUTPUT_DATA_RATE = 0xf0,
-    LOW_POWER_MODE = 0x8,
-    AXIS_ENABLE = 0x7,
-    
-    //REG_CTRL_2
-    HIGHPASS_FILTER_MODE = 0xc0,
-    HIGHPASS_FILTER_CUTOFF = 0x30,
-    FILTERED_DATA_SELECTION = 0x7,
-    HIGHPASS_FILTER_CLICK = 0x4,
-    HIGHPASS_AOI_INT2 = 0x2,
-    HIGHPASS_AOI_INT1 = 0x1,
-    
-    //REG_CTRL_3
-    INT1_CLICK = 0x80,
-    INT1_AOI1 = 0x40,
-    INT1_AOI2 = 0x20,
-    INT1_DRDY1 = 0x10,
-    INT1_DRDY2 = 0x8,
-    INT1_FIFO_WATERMARK = 0x4,
-    INT1_FIFO_OVERRUN = 0x2,
+	TEMPERATURE_DATA_AVAILABLE  = 0x4,
+	TEMPERATURE_ENABLE = 0xc0,
 
-    //REG_CTRL_4
-    BLOCKDATA_UPDATE = 0x80, //force atomic lsb and msb reads
-    ENDIANNESS_SELECTION = 0x40,
+	//REG_CTRL_1
+	OUTPUT_DATA_RATE = 0xf0,
+	LOW_POWER_MODE = 0x8,
+	AXIS_ENABLE = 0x7,
+
+	//REG_CTRL_2
+	HIGHPASS_FILTER_MODE = 0xc0,
+	HIGHPASS_FILTER_CUTOFF = 0x30,
+	FILTERED_DATA_SELECTION = 0x7,
+	HIGHPASS_FILTER_CLICK = 0x4,
+	HIGHPASS_AOI_INT2 = 0x2,
+	HIGHPASS_AOI_INT1 = 0x1,
+
+	//REG_CTRL_3
+	INT1_CLICK = 0x80,
+	INT1_AOI1 = 0x40,
+	INT1_AOI2 = 0x20,
+	INT1_DRDY1 = 0x10,
+	INT1_DRDY2 = 0x8,
+	INT1_FIFO_WATERMARK = 0x4,
+	INT1_FIFO_OVERRUN = 0x2,
+
+	//REG_CTRL_4
+	BLOCKDATA_UPDATE = 0x80, //force atomic lsb and msb reads
+	ENDIANNESS_SELECTION = 0x40,
 	FS_MASK = 0x30,
-    HIGHRES = 0x08,
-    SELFTEST_ENABLE = 0x06,
-    SPI_SERIAL_MODE = 0x01, //0 == 4 wire, 1 == 3 wire
-    
-    //REG_CTRL_5
-    BOOT = 0x80,
-    FIFO_EN = 0x40,
-    LATCH_INTERRUPT1 = 0x8, // INT1_SRC cleared by reading INT1_SRC only
-    LATCH_INTERRUPT2 = 0x2,
-    
-    //REG_CTRL_6
-    INT2_CLICK_ENABLE = 0x80,
-    INT1_OUTPUT_ON_LINE_2 = 0x40, //output interrupt generator 2 on interrupt line 1...
-    INT2_OUTPUT_ON_LINE_2 = 0x20,
-    BOOT_INT2 = 0x8, //boot on assertion of interrupt 2
-    INT_ACTIVE = 0x2, // 1 = active high, 0 = active low
-    
-    //REG_FIFO_CTRL
-    FIFO_MODE_SELECTION = 0xc0,
-    FIFO_TRIGGER_SELECTION = 0x40,
-    FIFO_WATERMARK_THRESHOLD = 0x1f,
-    
-    //REG_FIFO_SRC
-    FIFO_WATERMARK = 0x80,
-    FIFO_OVERRUN = 0x40,
-    
-    //REG_INT1_CFG
-    INT1_AND_OR = 0x80,
-    INT1_6D = 0x40,
-    INT1_Z_HIGH = 0x20,
-    INT1_Z_LOW = 0x10,
-    INT1_Y_HIGH = 0x8,
-    INT1_Y_LOW = 0x4,
-    INT1_X_HIGH = 0x2,
-    INT1_X_LOW = 0x1,
-    
-    //INT1_SRC
-    //... we will just look for not zero...
-    // reading this one will clear the interrupt in latched mode
-    
-    //INT1_THS
-    //just a number... 1lsb = 16mg at 2g
-    
-    //INT1_DURATION
-    //minimum duration to trigger interrupt 1...
-    
-    
-    //INT2, click won't be used.
+	HIGHRES = 0x08,
+	SELFTEST_ENABLE = 0x06,
+	SPI_SERIAL_MODE = 0x01, //0 == 4 wire, 1 == 3 wire
+
+	//REG_CTRL_5
+	BOOT = 0x80,
+	FIFO_EN = 0x40,
+	LATCH_INTERRUPT1 = 0x8, // INT1_SRC cleared by reading INT1_SRC only
+	LATCH_INTERRUPT2 = 0x2,
+
+	//REG_CTRL_6
+	INT2_CLICK_ENABLE = 0x80,
+	INT1_OUTPUT_ON_LINE_2 = 0x40, //output interrupt generator 2 on interrupt line 1...
+	INT2_OUTPUT_ON_LINE_2 = 0x20,
+	BOOT_INT2 = 0x8, //boot on assertion of interrupt 2
+	INT_ACTIVE = 0x2, // 1 = active high, 0 = active low
+
+	//REG_FIFO_CTRL
+	FIFO_MODE_SELECTION = 0xc0,
+	FIFO_TRIGGER_SELECTION = 0x40,
+	FIFO_WATERMARK_THRESHOLD = 0x1f,
+
+	//REG_FIFO_SRC
+	FIFO_WATERMARK = 0x80,
+	FIFO_OVERRUN = 0x40,
+
+	//REG_INT1_CFG
+	INT1_AND_OR = 0x80,
+	INT1_6D = 0x40,
+	INT1_Z_HIGH = 0x20,
+	INT1_Z_LOW = 0x10,
+	INT1_Y_HIGH = 0x8,
+	INT1_Y_LOW = 0x4,
+	INT1_X_HIGH = 0x2,
+	INT1_X_LOW = 0x1,
+
+	//INT1_SRC
+	//... we will just look for not zero...
+	// reading this one will clear the interrupt in latched mode
+
+	//INT1_THS
+	//just a number... 1lsb = 16mg at 2g
+
+	//INT1_DURATION
+	//minimum duration to trigger interrupt 1...
+
+
+	//INT2, click won't be used.
 };
