e6b2c1ec8ee8b115f57c7435767233662fb26690
Jackson Chen <jchen@hello.is>
2016-03-02 13:39:58 -0800

    reworked ant drive device parsing logic
    * make it simpler and consistent for both peripheral and central

--- ant/ant_driver.c
+++ ant/ant_driver.c
@@ -171,64 +171,41 @@ int32_t hlo_ant_cw_test(uint8_t freq, uint8_t tx_power){
     return 0;
 }
 
-static void
-_handle_rx(uint8_t channel, uint8_t * msg_buffer, uint16_t size){
-    ANT_MESSAGE * msg = (ANT_MESSAGE*)msg_buffer;
-    uint8_t * rx_payload = msg->ANT_MESSAGE_aucPayload;
-    hlo_ant_device_t device;
-    if(self.role == HLO_ANT_ROLE_CENTRAL){
+static bool _parse_device(uint8_t channel, uint8_t * msg_buffer, hlo_ant_device_t * out_device, hlo_ant_role role){
+    if(role == HLO_ANT_ROLE_CENTRAL){
+        ANT_MESSAGE * msg = (ANT_MESSAGE*)msg_buffer;
         EXT_MESG_BF ext = msg->ANT_MESSAGE_sExtMesgBF;
         uint8_t * extbytes = msg->ANT_MESSAGE_aucExtData;
         if(ext.ucExtMesgBF & MSG_EXT_ID_MASK){
-            device = (hlo_ant_device_t){
+            *out_device = (hlo_ant_device_t){
                 .device_number = *((uint16_t*)extbytes),
-                .device_type = extbytes[2],
-                .transmit_type = extbytes[3],
-                .measurement_type = extbytes[4],
-                .rssi = extbytes[5],
+                    .device_type = extbytes[2],
+                    .transmit_type = extbytes[3],
+                    .measurement_type = extbytes[4],
+                    .rssi = extbytes[5],
             };
-        }else{
-            //error
-            return;
-        }
-    }else if(self.role == HLO_ANT_ROLE_PERIPHERAL){
-        if(NRF_SUCCESS == sd_ant_channel_id_get(channel, &device.device_number, &device.device_type, &device.transmit_type)){
-        }else{
-            //error
-            return;
+            return true;
         }
     }else{
-        return;
-    }
-    self.event_listener->on_rx_event(&device, rx_payload, 8, self.role);
-
-    if(self.role == HLO_ANT_ROLE_CENTRAL){
-        uint8_t out_buf[8] = {0};
-        uint8_t out_size = 0;
-        self.event_listener->on_tx_event(&device, out_buf, &out_size, self.role);
-        if(out_size && out_size <= 8){
-            sd_ant_broadcast_message_tx(channel, out_size, out_buf);
+        if(NRF_SUCCESS == sd_ant_channel_id_get(channel, &out_device->device_number, &out_device->device_type, &out_device->transmit_type)){
+            return true;
         }
+
     }
+    return false;
+}
+static void
+_handle_rx(uint8_t * msg_buffer, uint16_t size, const hlo_ant_device_t * device){
+    ANT_MESSAGE * msg = (ANT_MESSAGE*)msg_buffer;
+    uint8_t * rx_payload = msg->ANT_MESSAGE_aucPayload;
+    self.event_listener->on_rx_event(device, rx_payload, 8, self.role);
 }
 
 static void  //peripheral tx mode
-_handle_tx(uint8_t channel){
-    hlo_ant_device_t device;
+_handle_tx(uint8_t channel, const hlo_ant_device_t * dev){
     uint8_t out_buf[8] = {0};
     uint8_t out_size = 0;
-    if(channel == 0 && self.role == HLO_ANT_ROLE_CENTRAL){
-        //this should not happen
-        return;
-    }else{
-        if(NRF_SUCCESS == sd_ant_channel_id_get(channel, &device.device_number, &device.device_type, &device.transmit_type)){
-
-        }else{
-            //error
-            return;
-        }
-    }
-    self.event_listener->on_tx_event(&device, out_buf, &out_size, self.role);
+    self.event_listener->on_tx_event(dev, out_buf, &out_size, self.role);
     if(out_size && out_size <= 8){
         sd_ant_broadcast_message_tx(channel, out_size, out_buf);
     }
@@ -238,12 +215,18 @@ void ant_handler(ant_evt_t * p_ant_evt){
     uint8_t event = p_ant_evt->event;
     uint8_t ant_channel = p_ant_evt->channel;
     uint8_t * event_message_buffer = (uint8_t*)p_ant_evt->evt_buffer;
+    hlo_ant_device_t dev;
     switch(event){
         case EVENT_RX_FAIL:
             break;
         case EVENT_RX:
             DEBUGS("R");
-            _handle_rx(ant_channel,event_message_buffer, ANT_EVENT_MSG_BUFFER_MIN_SIZE);
+            if( _parse_device(ant_channel, event_message_buffer, &dev, self.role) ){
+                _handle_rx(event_message_buffer, ANT_EVENT_MSG_BUFFER_MIN_SIZE, &dev);
+                if(self.role == HLO_ANT_ROLE_CENTRAL){
+                    _handle_tx(ant_channel, &dev);
+                }
+            }
             break;
         case EVENT_RX_SEARCH_TIMEOUT:
             DEBUGS("RXTO\r\n");
@@ -256,8 +239,10 @@ void ant_handler(ant_evt_t * p_ant_evt){
             break;
         case EVENT_TX:
             DEBUGS("T");
-            if(self.role == HLO_ANT_ROLE_PERIPHERAL){
-                _handle_tx(ant_channel);
+            if( _parse_device(ant_channel, event_message_buffer, &dev, self.role) ){
+                if(self.role == HLO_ANT_ROLE_PERIPHERAL){
+                    _handle_tx(ant_channel, &dev);
+                }
             }
             break;
         case EVENT_TRANSFER_TX_FAILED:
