22f5c657fb17013ae6b96d295975036d8d93f500
Jackson Chen <jchen@hello.is>
2016-08-09 10:46:05 +0800

    fix compiler error

--- morpheus/message_imu.c
+++ morpheus/message_imu.c
@@ -16,11 +16,15 @@ static MSG_Status _flush(void){
 }
 
 static MSG_Status _init(void){
+#ifdef PLATFORM_HAS_ACCEL_SPI
     if(0 == imu_init_simple(SPI_Channel_0, SPI_Mode3, ACCEL_MISO, ACCEL_MOSI, ACCEL_SCLK, ACCEL_nCS)){
         return SUCCESS;
     }else{
         return FAIL;
     }
+#else
+    return FAIL;
+#endif
 }
 static MSG_Status _send(MSG_Address_t src, MSG_Address_t dst, MSG_Data_t * data){
     return SUCCESS;
