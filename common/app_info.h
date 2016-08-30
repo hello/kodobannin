fa6b409e168313e29ee22ebd965a1c892d6680b9
Jackson Chen <jchen@hello.is>
2016-08-30 11:29:49 -0700

    update version

--- common/app_info.h
+++ common/app_info.h
@@ -3,8 +3,8 @@
  * A place to put all common defines
  */
 //make sure the FW_VERSION_STRING fits in MorpheusCommand.top_version (16 bytes)
-#define FW_VERSION_STRING "1.4.3"
+#define FW_VERSION_STRING "1.5.0"
 
 //pill only
-#define FIRMWARE_VERSION_8BIT (43)
+#define FIRMWARE_VERSION_8BIT (50)
 
