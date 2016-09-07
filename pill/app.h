ff6bdbd3ca75dbf2a56e39e1b02b0d8bb6276047
Jackson Chen <jchen@hello.is>
2016-09-07 13:52:16 -0700

    reduce sleep timeout

--- pill/app.h
+++ pill/app.h
@@ -112,7 +112,7 @@ enum {
 // Battery level measurement interval (ticks).
 #define BATTERY_LEVEL_MEAS_INTERVAL          APP_TIMER_TICKS(200000, APP_TIMER_PRESCALER)
 #define IMU_ACTIVE_INTERVAL                  APP_TIMER_TICKS(500, APP_TIMER_PRESCALER)
-#define IMU_SLEEP_TIMEOUT                   (IMU_ACTIVE_INTERVAL * 6) /* time which the imu goes back to sleep */
+#define IMU_SLEEP_TIMEOUT                   (IMU_ACTIVE_INTERVAL * 3) /* time which the imu goes back to sleep */
 #define LED_INIT_LIGHTUP_INTERAVL            APP_TIMER_TICKS(200,APP_TIMER_PRESCALER)
 #define HEARTBEAT_INTERVAL_MIN              (3600/60)
 #define BATT_MEASURE_INTERVAL_MIN           (1200/60)  /* Measure battery level every 20 minutes */
