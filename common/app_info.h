e6968ba0de4411987f78365989c389d3d95229d4
Jackson Chen <jchen@hello.is>
2016-11-28 15:43:07 -0800

    update version

--- common/app_info.h
+++ common/app_info.h
@@ -3,10 +3,10 @@
  * A place to put all common defines
  */
 //make sure the FW_VERSION_STRING fits in MorpheusCommand.top_version (16 bytes)
-#define FW_VERSION_STRING "1.8.1"
+#define FW_VERSION_STRING "1.8.2"
 
 //pill only
-#define FIRMWARE_VERSION_8BIT (0x51)
+#define FIRMWARE_VERSION_8BIT (0x52)
 
 #define BLE_SIG_COMPANY_ID 1002
 
